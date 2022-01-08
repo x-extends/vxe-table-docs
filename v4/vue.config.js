95a0152397e4a9b2c3ebe1cdc8064138c140f43f
xuliangzhan <xu_liangzhan@163.com>
2022-01-08 22:00:41 +0800

    update

--- v4/vue.config.js
+++ v4/vue.config.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'production') {
   publicPath = `/vxe-table/v${version}/`
   if (process.env.npm_lifecycle_event === 'build:main') {
     publicPath = `/v${version}/`
-    process.env.VUE_APP_CDN_URL = `/v${version}/`
+    process.env.VUE_APP_CDN_URL = '/'
     process.env.VUE_APP_MAIN_URL = '/'
   }
 }
