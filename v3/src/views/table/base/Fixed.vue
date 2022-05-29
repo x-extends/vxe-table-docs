09018646bc18867a94dce856134e3d21318f5e7e
BothEyes <1993046671@qq.com>
2022-05-29 23:31:33 +0800

    feat:添加固定列的优化提示

--- v3/src/views/table/base/Fixed.vue
+++ v3/src/views/table/base/Fixed.vue
@@ -2,7 +2,8 @@
   <div>
     <p class="tip">
       固定列，通过设置 <table-column-api-link prop="fixed"/>=left|right 来启用固定列<br>
-      当横向内容过多时，将列固定在左右两侧
+      当横向内容过多时，将列固定在左右两侧<br>
+      <span class="red">（注：固定列建议设置 <table-api-link prop="show-overflow"/> 该值，启用优化模式大幅提升渲染速度，亦可防止固定列里面的组件重复渲染等问题 ）</span>
     </p>
 
     <vxe-table
