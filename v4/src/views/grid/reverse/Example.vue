51adf669260dc98dddd93065cf128ab9bc227913
xuliangzhan <xu_liangzhan@163.com>
2023-05-07 18:09:55 +0800

    update docs

---
+++ v4/src/views/grid/reverse/Example.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <CodeLight path="grid/reverse/Demo1">
+      <template #tip>
+        <div>反转表格，只需要将 <grid-api-link prop="columns"/> 和 <grid-api-link prop="data"/> 数据进行反转<span class="red">（具体请自行实现，该示例仅供参考）</span></div>
+      </template>
+    </CodeLight>
+
+    <CodeLight path="grid/reverse/Demo2">
+      <template #tip>
+        <div>行和列数据转换<span class="red">（具体请自行实现，该示例仅供参考）</span></div>
+      </template>
+    </CodeLight>
+  </div>
+</template>
---
+++ v4/src/views/table/advanced/print/Demo1.vue
@@ -0,0 +1,146 @@
+<template>
+  <div>
+    <vxe-toolbar>
+      <template #buttons>
+        <vxe-button @click="printEvent1">打印</vxe-button>
+        <vxe-button @click="printSelectEvent">打印选中</vxe-button>
+        <vxe-button @click="printEvent2">高级打印</vxe-button>
+      </template>
+    </vxe-toolbar>
+
+    <vxe-table
+      border
+      show-footer
+      ref="xTable"
+      height="500"
+      :print-config="printConfig"
+      :footer-method="footerMethod"
+      :data="tableData">
+      <vxe-column type="checkbox" width="60"></vxe-column>
+      <vxe-column type="seq" width="60"></vxe-column>
+      <vxe-colgroup title="Group1">
+        <vxe-column field="name" title="Name"></vxe-column>
+        <vxe-colgroup title="Group2">
+          <vxe-column field="role" title="Role"></vxe-column>
+          <vxe-column field="age" title="Age"></vxe-column>
+        </vxe-colgroup>
+        <vxe-column field="address" title="Address"></vxe-column>
+      </vxe-colgroup>
+    </vxe-table>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import { VxeTableInstance, VxeTablePropTypes, VxeButtonEvents } from 'vxe-table'
+
+interface RowVO {
+  id: number
+  name: string
+  nickname: string
+  role: string
+  sex: string
+  age: number
+  address: string
+}
+
+const tableData = ref<RowVO[]>([
+  { id: 10001, name: 'Test1', nickname: 'T1', role: 'Develop', sex: 'Man', age: 28, address: 'test abc' },
+  { id: 10002, name: 'Test2', nickname: 'T2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou' },
+  { id: 10003, name: 'Test3', nickname: 'T3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
+  { id: 10004, name: 'Test4', nickname: 'T4', role: 'Designer', sex: 'Women', age: 23, address: 'test abc' },
+  { id: 10005, name: 'Test5', nickname: 'T5', role: 'Develop', sex: 'Women', age: 30, address: 'Shanghai' },
+  { id: 10006, name: 'Test6', nickname: 'T6', role: 'Designer', sex: 'Women', age: 21, address: 'test abc' },
+  { id: 10007, name: 'Test7', nickname: 'T7', role: 'Test', sex: 'Man', age: 29, address: 'test abc' },
+  { id: 10008, name: 'Test8', nickname: 'T8', role: 'Develop', sex: 'Man', age: 35, address: 'test abc' }
+])
+
+const printConfig = reactive<VxeTablePropTypes.PrintConfig>({
+  // 自定义打印的样式示例
+  style: `
+    .vxe-table {
+      color: #000000; // 修改表格默认颜色
+      font-size: 12px; // 修改表格默认字体大小
+      font-family: "Microsoft YaHei",微软雅黑,"MicrosoftJhengHei",华文细黑,STHeiti,MingLiu; // 修改表格默认字体
+    }
+    .vxe-table,
+    .vxe-table thead th,
+    .vxe-table tbody td,
+    .vxe-table tfoot td  {
+      border-color: #000000; // 修改表格边框颜色
+    }
+    .vxe-table thead th {
+      color: green; // 修改表头字体颜色
+      font-size: 14px; // 修改表头默认字体大小
+    }
+    .vxe-table tfoot td {
+      color: red; // 修改表尾字体颜色
+    }
+    `
+})
+
+const xTable = ref<VxeTableInstance>()
+
+const printEvent1: VxeButtonEvents.Click = () => {
+  const $table = xTable.value
+  if ($table) {
+    $table.print()
+  }
+}
+
+const printSelectEvent: VxeButtonEvents.Click = () => {
+  const $table = xTable.value
+  if ($table) {
+    $table.print({
+      data: $table.getCheckboxRecords()
+    })
+  }
+}
+
+const printEvent2: VxeButtonEvents.Click = () => {
+  const $table = xTable.value
+  if ($table) {
+    $table.openPrint()
+  }
+}
+
+const meanNum = (list: any[], field: string) => {
+  let count = 0
+  list.forEach(item => {
+    count += Number(item[field])
+  })
+  return count / list.length
+}
+
+const sumNum = (list: any[], field: string) => {
+  let count = 0
+  list.forEach(item => {
+    count += Number(item[field])
+  })
+  return count
+}
+
+const footerMethod: VxeTablePropTypes.FooterMethod = ({ columns, data }) => {
+  const footerData = [
+    columns.map((column, columnIndex) => {
+      if (columnIndex === 0) {
+        return '平均'
+      }
+      if (['age', 'rate'].includes(column.field)) {
+        return meanNum(data, column.field)
+      }
+      return null
+    }),
+    columns.map((column, columnIndex) => {
+      if (columnIndex === 0) {
+        return '和值'
+      }
+      if (['age', 'rate'].includes(column.field)) {
+        return sumNum(data, column.field)
+      }
+      return null
+    })
+  ]
+  return footerData
+}
+</script>
