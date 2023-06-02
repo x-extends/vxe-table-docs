ac09aebb051717e608ebc33c4168e2c19b48d56e
xuliangzhan <xu_liangzhan@163.com>
2023-06-02 08:33:35 +0800

    update docs

---
+++ v4/src/views/table/base/current/Demo1.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <p>
+      <vxe-button @click="selectRowEvent">设置第二行选中</vxe-button>
+      <vxe-button @click="clearSelectEvent">取消选中</vxe-button>
+      <vxe-button @click="getCurrentEvent">获取高亮行</vxe-button>
+    </p>
+
+    <vxe-table
+      border
+      ref="tableRef"
+      height="300"
+      :row-config="{isCurrent: true, isHover: true}"
+      :data="tableData"
+      @current-change="currentChangeEvent">
+      <vxe-column field="name" title="Name"></vxe-column>
+      <vxe-column field="sex" title="Sex"></vxe-column>
+      <vxe-column field="age" title="Age"></vxe-column>
+      <vxe-column field="address" title="Address" show-overflow></vxe-column>
+    </vxe-table>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { VXETable, VxeTableInstance, VxeTableEvents } from 'vxe-table'
+
+interface RowVO {
+  name: string
+  role: string
+  sex: string
+  age: number
+  content: string
+}
+
+const tableRef = ref<VxeTableInstance<RowVO>>()
+
+const tableData = ref<RowVO[]>([
+  { name: 'Test1', role: 'Develop', sex: 'Man', age: 28, content: 'test abc' },
+  { name: 'Test2', role: 'Test', sex: 'Women', age: 41, content: 'Guangzhou' },
+  { name: 'Test3', role: 'PM', sex: 'Man', age: 32, content: 'Shanghai' },
+  { name: 'Test4', role: 'Designer', sex: 'Women', age: 24, content: 'Shanghai' }
+])
+
+const currentChangeEvent: VxeTableEvents.CurrentChange<RowVO> = ({ rowIndex }) => {
+  console.log(`行选中事件 ${rowIndex}`)
+}
+
+const selectRowEvent = () => {
+  const $table = tableRef.value
+  if ($table) {
+    $table.setCurrentRow(tableData.value[1])
+  }
+}
+
+const clearSelectEvent = () => {
+  const $table = tableRef.value
+  if ($table) {
+    $table.clearCurrentRow()
+  }
+}
+
+const getCurrentEvent = () => {
+  const $table = tableRef.value
+  if ($table) {
+    VXETable.modal.alert(JSON.stringify($table.getCurrentRecord()))
+  }
+}
+</script>
---
+++ v4/src/views/table/base/current/Example.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <CodeLight path="table/base/current/Demo1">
+      <template #tip>
+        <div>使用 <table-api-link prop="row-config"/>.<table-api-link prop="isCurrent"/> 显示高亮行，当前行是唯一的；用户操作点击选项时会触发事件 <table-api-link prop="current-change"/></div>
+      </template>
+    </CodeLight>
+  </div>
+</template>
