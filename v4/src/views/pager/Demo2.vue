37b87a2baa1ce01c9b32f87f3f56bf6da4410793
xuliangzhan <xu_liangzhan@163.com>
2023-06-18 21:42:28 +0800

    update docs

---
+++ v4/src/views/pager/Demo2.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <vxe-pager
+      v-model:current-page="pageVO1.currentPage"
+      v-model:page-size="pageVO1.pageSize"
+      :total="pageVO1.total"
+      :layouts="['PrevJump', 'PrevPage', 'Number', 'NextPage', 'NextJump', 'Sizes', 'FullJump', 'Total']">
+    </vxe-pager>
+
+    <vxe-pager
+      background
+      v-model:current-page="pageVO2.currentPage"
+      v-model:page-size="pageVO2.pageSize"
+      :total="pageVO2.total"
+      :layouts="['PrevJump', 'PrevPage', 'JumpNumber', 'NextPage', 'NextJump', 'Sizes', 'FullJump', 'Total']">
+    </vxe-pager>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+const pageVO1 = reactive({
+  currentPage: 1,
+  pageSize: 30,
+  total: 8
+})
+
+const pageVO2 = reactive({
+  currentPage: 1,
+  pageSize: 30,
+  total: 8
+})
+</script>
