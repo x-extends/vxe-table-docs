a129a9dc7a3ddec84e2473624bc531801698ca2d
xuliangzhan <xu_liangzhan@163.com>
2023-08-11 08:26:25 +0800

    update docs

---
+++ v4/src/views/table/scroll/partialLoad/Example.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <CodeLight path="table/scroll/partialLoad/Demo1">
+      <template #tip>
+        <div>局部递增数据</div>
+      </template>
+    </CodeLight>
+  </div>
+</template>
