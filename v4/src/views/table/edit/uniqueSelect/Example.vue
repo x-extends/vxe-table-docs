df796bbb207a4e3db76ddb00d69042ede115873a
xuliangzhan <xu_liangzhan@163.com>
2023-08-30 11:10:56 +0800

    update docs

---
+++ v4/src/views/table/edit/uniqueSelect/Example.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <CodeLight path="table/edit/uniqueSelect/Demo1">
+      <template #tip>
+        <div>实现简单的唯一下拉选项列表<span class="red">（具体请自行实现，该示例仅供参考）</span></div>
+      </template>
+    </CodeLight>
+  </div>
+</template>
