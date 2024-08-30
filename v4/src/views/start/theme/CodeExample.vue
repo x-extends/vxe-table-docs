eb599a27f11cec64c083e5541acec1ec300dc7ae
xuliangzhan <xlz26296x@163.com>
2024-08-30 09:54:04 +0800

    update docs

--- v4/src/views/start/theme/CodeExample.vue
+++ v4/src/views/start/theme/CodeExample.vue
@@ -2,21 +2,40 @@
   <div>
     <CodeLight>
       <template #tip>
-        <vxe-tip status="primary" title="自定义主题">
-          默认支持 light 和 dark 两种主题样式，通过调用 VxeUI.setTheme('light') 方法修改组件主题样式
+        <vxe-tip status="primary" title="内置主题">
+          默认支持 light 和 dark 两种主题样式，通过调用 VxeUI.setTheme('light') 方式修改组件主题样式
         </vxe-tip>
       </template>
 
       <template #use>
         <pre>
           <pre-code class="javascript">
-            import { VXETable } from 'vxe-table'
+            import { VxeUI } from 'vxe-pc-ui'
 
             // 切换为默认主题
-            // VXETable.setTheme('light')
+            // VxeUI.setTheme('light')
 
             // 切换为暗黑主题
-            VXETable.setTheme('dark')
+            VxeUI.setTheme('dark')
+          </pre-code>
+        </pre>
+      </template>
+    </CodeLight>
+
+    <CodeLight>
+      <template #tip>
+        <vxe-tip status="primary" title="自定义主题">
+          通过修复 css 变量（<vxe-link href="https://gitee.com/xuliangzhan/vxe-table/blob/master/styles/theme/light.scss" status="primary">全量变量</vxe-link>）方式修改组件主题样式，给 html 加个 class="my-theme"，示例：
+        </vxe-tip>
+      </template>
+
+      <template #use>
+        <pre>
+          <pre-code class="css">
+            html.my-theme {
+              // 主题色
+              --vxe-ui-font-primary-color: #44bf16;
+            }
           </pre-code>
         </pre>
       </template>
