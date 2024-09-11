e4c2169a2d3d9eff906c7f3fe195727acd5af914
xuliangzhan <xlz26296x@163.com>
2024-09-11 21:09:15 +0800

    update docs

--- v4.6/gulpfile.js
+++ v4.6/gulpfile.js
@@ -60,7 +60,7 @@ gulp.task('handle_vue_tstojs', gulp.series('handle_vue_tmpltojs', () => {
       const handlePath = `${this.file.dirname}\\${this.file.basename}`.replace('\\src\\', '\\temp\\').replace('.vue', '.js')
       if (fs.existsSync(handlePath)) {
         return text.replace(/<script [^>]*?>([\s\S]*)<\/script>/, () => {
-          let codeContent = fs.readFileSync(handlePath, 'utf-8').replace('use strict', '')
+          let codeContent = fs.readFileSync(handlePath, 'utf-8')
           if (codeContent) {
             codeContent += '\n'
           }
