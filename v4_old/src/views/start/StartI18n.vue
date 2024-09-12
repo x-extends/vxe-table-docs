56a2ce3a4c2aabdf18b33045535b8ba1b5054edf
xuliangzhan <xlz26296x@163.com>
2024-09-12 12:46:35 +0800

    update docs

--- v4_old/src/views/start/StartI18n.vue
+++ v4_old/src/views/start/StartI18n.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <h2>{{ $t('app.aside.nav.i18n') }}</h2>
-    <p class="tip">{{ $t('app.body.demo.start.i18n.i18nTitle') }}</p>
+    <p class="tip">使用内置国际化功能，通过调用 setLanguage('zh-CN') 方法可以切换组件语言</p>
     <pre>
       <pre-code class="typescript">{{ demoCodes[0] }}</pre-code>
+    </pre>
+    <p class="tip">当使用其他国际化插件时，例如 vxe-i18n，可以使用自定义国际化</p>
+    <pre>
+      <pre-code class="typescript">{{ demoCodes[1] }}</pre-code>
+      <pre-code class="typescript">{{ demoCodes[2] }}</pre-code>
     </pre>
   </div>
 </template>
@@ -39,6 +44,50 @@ export default defineComponent({
 
         // 切换语言
         VXETable.setLanguage('zh-CN')
+        `,
+        `
+        import { createI18n } from 'vue-i18n'
+        import zhCN from 'vxe-table/lib/language/zh-CN'
+        // import zhHK from 'vxe-table/lib/language/zh-HK'
+        // import zhTW from 'vxe-table/lib/language/zh-TW'
+        // import zhMO from 'vxe-table/lib/language/zh-MO'
+        import enUS from 'vxe-table/lib/language/en-US'
+        // import jaJP from 'vxe-table/lib/language/ja-JP'
+        // import esES from 'vxe-table/lib/language/es-ES'
+        // import ptBR from 'vxe-table/lib/language/pt-BR'
+        // import viVN from 'vxe-table/lib/language/vi-VN'
+        // import koKR from 'vxe-table/lib/language/ko-KR'
+        // import huHU from 'vxe-table/lib/language/hu-HU'
+        // import ruRU from 'vxe-table/lib/language/ru-RU'
+
+        const messages = {
+          zh_CN: {
+            ...zhCN
+          },
+          en_US: {
+            ...enUS
+          }
+        }
+
+        const i18n = createI18n({
+          locale: 'zh_CN',
+          messages,
+        })
+
+        export default i18n
+        `,
+        `
+        // ...
+        import i18n from './i18n'
+        import { VXETable } from 'vxe-table'
+
+        VXETable.setConfig({
+          // 对组件内置的提示语进行国际化翻译
+          i18n: (key, args) => i18n.global.t(key, args)
+        })
+
+        // 切换指定语言
+        i18n.locale = 'en_US'
         `
       ]
     }
