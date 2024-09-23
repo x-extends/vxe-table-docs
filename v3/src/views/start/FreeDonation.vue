ef9b0cc736ca86ddad33635c08d94c504e961db1
xuliangzhan <xlz26296x@163.com>
2024-09-23 18:06:31 +0800

    update docs

--- v3/src/views/start/FreeDonation.vue
+++ v3/src/views/start/FreeDonation.vue
@@ -9,10 +9,32 @@
       <div><vxe-link status="primary" :href="`https://github.com/x-extends/${packName}`" target="_blank">{{ pageTitle }}</vxe-link> 是 MIT 开源的，使用完全免费。为了使项目能够健康持续的发展下去，您可以通过下方扫码来支持作者。</div>
     </vxe-tip>
     <vxe-tip title="发展历史" status="success">
-      <div>2016 开源了 javascript 工具类 <vxe-link status="primary" href="https://github.com/x-extends/xe-utils" target="_blank">xe-utils</vxe-link>、请求库 <vxe-link status="primary" href="https://github.com/x-extends/xe-ajax" target="_blank">xe-ajax</vxe-link></div>
-      <div>2018 开源了表格库 <vxe-link status="primary" href="https://github.com/x-extends/vxe-table" target="_blank">vxe-table</vxe-link></div>
-      <div>2024 开源了组件库 <vxe-link status="primary" href="https://github.com/x-extends/vxe-pc-ui" target="_blank">vxe-pc-ui</vxe-link>（从 Vxe Table 中分离出组件，组成 Vxe UI）</div>
-      <div>敬请期待：Admin 系统模板、低代码系统模板、移动端 H5 、小程序组件库、数据图表 </div>
+      <div>
+        2016 开源了 javascript 工具类 <vxe-link status="primary" href="https://vxeui.com/xe-utils" target="_blank">xe-utils</vxe-link>
+        <vxe-link status="primary" icon="vxe-icon-github-fill" href="https://github.com/x-extends/xe-utils" target="_blank"></vxe-link>
+        <vxe-link status="error" icon="vxe-icon-gitee-fill" href="https://gitee.com/x-extends/xe-utils" target="_blank"></vxe-link>
+      </div>
+      <div>
+        2016 开源了异步请求库 <vxe-link status="primary" href="https://vxeui.com/xe-utils" target="_blank">xe-ajax</vxe-link>
+        <vxe-link status="primary" icon="vxe-icon-github-fill" href="https://github.com/x-extends/xe-ajax" target="_blank"></vxe-link>
+        <vxe-link status="error" icon="vxe-icon-gitee-fill" href="https://gitee.com/x-extends/xe-ajax" target="_blank"></vxe-link>
+      </div>
+      <div>
+        2018 开源了表格库 <vxe-link status="primary" href="https://vxetable.cn" target="_blank">vxe-table</vxe-link>
+        <vxe-link status="primary" icon="vxe-icon-github-fill" href="https://github.com/x-extends/vxe-table" target="_blank"></vxe-link>
+        <vxe-link status="error" icon="vxe-icon-gitee-fill" href="https://gitee.com/xuliangzhan/vxe-table" target="_blank"></vxe-link>
+      </div>
+      <div>
+        2024 开源了组件库 <vxe-link status="primary" href="https://vxeui.com" target="_blank">vxe-pc-ui</vxe-link>
+        <vxe-link status="primary" icon="vxe-icon-github-fill" href="https://github.com/x-extends/vxe-pc-u" target="_blank"></vxe-link>
+        <vxe-link status="error" icon="vxe-icon-gitee-fill" href="https://gitee.com/x-extends/vxe-pc-u" target="_blank"></vxe-link>
+      </div>
+      <div>
+        2024 开源了后台系统系统模板 <vxe-link status="primary" href="https://vxeui.com/vxe-admin-template/" target="_blank">vxe-admin-template</vxe-link>
+        <vxe-link status="primary" icon="vxe-icon-github-fill" href="https://github.com/x-extends/vxe-admin-template" target="_blank"></vxe-link>
+        <vxe-link status="error" icon="vxe-icon-gitee-fill" href="https://gitee.com/x-extends/vxe-admin-template" target="_blank"></vxe-link>
+      </div>
+      <div>敬请期待：低代码系统模板、移动端 H5 、小程序组件库、数据图表 </div>
     </vxe-tip>
 
     <vxe-tabs v-model="supportAuthor" >
