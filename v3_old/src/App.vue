7a49c94a33c303ae9bbe4c819db435f7316fdfed
xuliangzhan <xlz26296x@163.com>
2024-09-29 20:45:23 +0800

    update docs

--- v3_old/src/App.vue
+++ v3_old/src/App.vue
@@ -74,7 +74,7 @@
             <template v-if="apiList.length">
               <ul class="nav-menu">
                 <li v-for="(item, index) in apiList" :key="index" :class="{expand: item.expand}">
-                  <a v-if="item.linkUrl" :href="item.linkUrl" target="_blank" class="link" style="padding-left: 20px;color: #409eff;">更多组件</a>
+                  <a v-if="item.linkUrl" :href="item.linkUrl" :target="item.linkTarget || '_blank'" class="link" style="padding-left: 20px;color: #409eff;">更多组件</a>
                   <a v-else class="nav-link" @click="linkEvent(item)" :title="item.disabled ? $t('app.body.other.newFunc') : item.label" :class="{'is-new': item.isNew, disabled: item.disabled, active: pageKey === item.value}">
                     <i class="vxe-icon-arrow-right nav-link-icon"></i>
                     <span v-html="item.label"></span>
@@ -82,7 +82,7 @@
                   <ul v-if="item.children" v-show="item.expand" class="nav-child-menu">
                     <li v-for="(child, cIndex) in item.children" :key="cIndex" :class="{'is-donation': child.locat && ['Donation'].includes(child.locat.name)}">
                       <a class="nav-link disabled" v-if="child.disabled" :title="$t('app.body.other.newFunc')" v-html="child.label"></a>
-                      <a class="nav-link" v-else-if="child.url" :href="child.url" :title="$t('app.body.other.newFunc')" v-html="child.label" target="_blank"></a>
+                      <a class="nav-link" v-else-if="child.url" :href="child.url" :title="$t('app.body.other.newFunc')" v-html="child.label" :target="child.linkTarget || '_blank'"></a>
                       <router-link v-else class="nav-link" :to="child.locat" :title="child.label" v-html="child.label"></router-link>
                     </li>
                   </ul>
