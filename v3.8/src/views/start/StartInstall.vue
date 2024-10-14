8ebfbd7f601b02ffd846dde1bd68b7815ed18c47
xuliangzhan <xlz26296x@163.com>
2024-10-14 18:45:42 +0800

    update docs

--- v3.8/src/views/start/StartInstall.vue
+++ v3.8/src/views/start/StartInstall.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     <h2>{{ $t('app.aside.nav.install') }}</h2>
+    <div class="red">需要注意：v3.0 可以直接升级 v3.8</div>
+    <div class="red">需要注意：v3.8 不能直接升级 v3.9，需要调整安装方式，如果是使用老版本记得锁定版本号</div>
+    <div style="color: #21c966;padding: 16px;margin: 16px 0;border: 2px solid #21c966;font-size: 1.2em;">
+      <div>锁定版本方式，修改 package.json</div>
+      <div>手动将对应的版本号修改为 "vxe-table": "~3.8.25"</div>
+    </div>
     <p class="tip">
       推荐使用 npm 的方式安装，它能更好地和 <a class="link" href="https://webpack.js.org/">webpack</a>、<a class="link" href="https://vitejs.dev/">vite</a> 等 打包工具配合使用。<br>
       依赖库： <a class="link" href="https://vxetable.cn/xe-utils" target="_blank">xe-utils</a>&nbsp;&nbsp;<a class="link" href="https://cn.vuejs.org/v2/guide/components-slots.html#%E5%8A%A8%E6%80%81%E6%8F%92%E6%A7%BD%E5%90%8D">vue 2.6+</a><br>
     </p>
     <pre>
       <pre-code class="shell">
-        npm install vxe-table@3.8.24
+        npm install vxe-table@3.8.25
       </pre-code>
       <pre-code class="javascript">
         import Vue from 'vue'
@@ -25,12 +31,12 @@
     <pre>
       <pre-code class="xml">
         &lt;!-- 引入样式 --&gt;
-        &lt;link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/vxe-table@3.8.24/lib/style.css"&gt;
+        &lt;link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/vxe-table@3.8.25/lib/style.css"&gt;
         &lt;!-- 引入vue --&gt;
         &lt;script src="https://cdn.jsdelivr.net/npm/vue@2.6.14"&gt;&lt;/script&gt;
         &lt;!-- 引入组件 --&gt;
         &lt;script src="https://cdn.jsdelivr.net/npm/xe-utils"&gt;&lt;/script&gt;
-        &lt;script src="https://cdn.jsdelivr.net/npm/vxe-table@3.8.24"&gt;&lt;/script&gt;
+        &lt;script src="https://cdn.jsdelivr.net/npm/vxe-table@3.8.25"&gt;&lt;/script&gt;
       </pre-code>
     </pre>
   </div>
--- v4.6/src/views/start/StartInstall.vue
+++ v4.6/src/views/start/StartInstall.vue
@@ -3,15 +3,19 @@
     <h2>{{ $t('app.aside.nav.install') }}</h2>
     <div class="red">需要注意：v4.0 可以直接升级 v4.6</div>
     <div class="red">需要注意：v4.6 不能直接升级 v4.7，需要调整安装方式，如果是使用老版本记得锁定版本号</div>
+    <div style="color: #21c966;padding: 16px;margin: 16px 0;border: 2px solid #21c966;font-size: 1.2em;">
+      <div>锁定版本方式，修改 package.json</div>
+      <div>手动将对应的版本号修改为 "vxe-table": "~4.6.21"</div>
+    </div>
     <p class="tip">
       推荐使用 npm 的方式安装，它能更好地和 <a class="link" href="https://webpack.js.org/">webpack</a>、<a class="link" href="https://vitejs.dev/">vite</a> 等打包工具配合使用。<br>
       依赖库： <a class="link" href="https://vxetable.cn/xe-utils" target="_blank">xe-utils</a>&nbsp;&nbsp;<a class="link" href="https://cn.vuejs.org/v2/guide/components-slots.html#%E5%8A%A8%E6%80%81%E6%8F%92%E6%A7%BD%E5%90%8D">vue 3.2+</a><br>
     </p>
     <pre>
       <pre-code class="shell">
-        npm install vxe-table@4.6.20
+        npm install vxe-table@4.6.21
         # 或者
-        yarn add vxe-table@4.6.20
+        yarn add vxe-table@4.6.21
       </pre-code>
       <pre-code class="typescript">{{ demoCodes[0] }}</pre-code>
     </pre>
@@ -23,12 +27,12 @@
     <pre>
       <pre-code class="xml">
         &lt;!-- 引入样式 --&gt;
-        &lt;link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/vxe-table@4.6.20/lib/style.css"&gt;
+        &lt;link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/vxe-table@4.6.21/lib/style.css"&gt;
         &lt;!-- 引入vue --&gt;
         &lt;script src="https://cdn.jsdelivr.net/npm/vue"&gt;&lt;/script&gt;
         &lt;!-- 引入组件 --&gt;
         &lt;script src="https://cdn.jsdelivr.net/npm/xe-utils"&gt;&lt;/script&gt;
-        &lt;script src="https://cdn.jsdelivr.net/npm/vxe-table@4.6.20"&gt;&lt;/script&gt;
+        &lt;script src="https://cdn.jsdelivr.net/npm/vxe-table@4.6.21"&gt;&lt;/script&gt;
       </pre-code>
     </pre>
   </div>
