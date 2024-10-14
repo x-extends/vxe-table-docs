0b4ddc65c7c6f42fcf7856737b8433e5be341e98
xuliangzhan <xlz26296x@163.com>
2024-10-14 10:36:35 +0800

    update docs

--- v3/src/components/ApiLink.vue
+++ v3/src/components/ApiLink.vue
@@ -1,5 +1,6 @@
 <template>
-  <vxe-link :router-link="{name: 'DocsApi', params: { name }, query: { q: q || prop }}" :content="q || prop" />
+  <vxe-link v-if="redirect && !hasUIDoMain" :href="`https://vxeui.com/#/${name}/api?q=${q || prop || ''}`" :content="q || prop || `vxe-${name}`"></vxe-link>
+  <vxe-link v-else :router-link="{name: 'DocsApi', params: { name }, query: { q: q || prop || '' }}" :content="q || prop || `vxe-${name}`" />
 </template>
 
 <script lang="ts">
@@ -9,7 +10,13 @@ export default Vue.extend({
   props: {
     name: String,
     q: String,
-    prop: String
+    prop: String,
+    redirect: Boolean
+  },
+  computed: {
+    hasUIDoMain () {
+      return location.origin.indexOf('vxeui') > -1
+    }
   }
 })
 </script>
--- v4/src/components/ApiLink.vue
+++ v4/src/components/ApiLink.vue
@@ -1,11 +1,19 @@
 <template>
-  <vxe-link :router-link="{name: 'DocsApi', params: { name }, query: { q: q || prop }}" :content="q || prop" />
+  <vxe-link v-if="redirect && !hasUIDoMain" :href="`https://vxeui.com/#/${name}/api?q=${q || prop || ''}`" :content="q || prop || `vxe-${name}`"></vxe-link>
+  <vxe-link v-else :router-link="{name: 'DocsApi', params: { name }, query: { q: q || prop || '' }}" :content="q || prop || `vxe-${name}`" />
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue'
+
 defineProps({
   name: String,
   q: String,
-  prop: String
+  prop: String,
+  redirect: Boolean
+})
+
+const hasUIDoMain = computed(() => {
+  return location.origin.indexOf('vxeui') > -1
 })
 </script>
