a10e7c7e2f2405df8b898796fc1c699925d5e65b
xuliangzhan <xlz26296x@163.com>
2024-10-17 20:13:30 +0800

    update docs

--- v3/src/views/start/install/CodeExample.vue
+++ v3/src/views/start/install/CodeExample.vue
@@ -6,42 +6,62 @@
           <div>可以配合 <vxe-link  href="https://webpack.js.org/" target="_blank">webpack</vxe-link> 等打包工具配合使用。</div>
           <div>依赖库： <vxe-link  href="https://v2.cn.vuejs.org/" target="_blank">vue 2.6+</vxe-link></div>
         </vxe-tip>
-        <vxe-tip status="error" title="需要注意！！！">
-          <div>v3.0 可以直接升级 v3.8（如果是使用老版本记得锁定版本号）</div>
-          <div>v3.8 不能直接升级 v3.9（组件代码基本不用改，变动的是安装方式、全局的主题变量）</div>
-        </vxe-tip>
       </template>
 
       <template #use>
+        <vxe-tip status="success" title="方式1.完整安装表格和配套 UI 库">
+          搭配强大的 UI 库可以使用全功能组件
+        </vxe-tip>
         <pre>
           <pre-code
             language="shell"
             :content="`
-              npm install ${ tableCDNLib }
-              # 或者
-              yarn add ${ tableCDNLib }
+            npm install ${uiCDNLib } ${ tableCDNLib }
+            # 或者
+            yarn add ${uiCDNLib } ${ tableCDNLib }
+            # 或者
+            pnpm add ${uiCDNLib } ${ tableCDNLib }
             `">
           </pre-code>
           <pre-code
             language="javascript"
+            content="
+            // ...
+            import VxeUI from 'vxe-pc-ui'
+            import 'vxe-pc-ui/lib/style.css'
+            import VxeUITable from 'vxe-table'
+            import 'vxe-table/lib/style.css'
+            // ...
+
+            Vue.use(VxeUI)
+            Vue.use(VxeUITable)
+            // ...">
+          </pre-code>
+        </pre>
+        <vxe-tip status="success" title="方式2.安装纯表格">
+          仅可使用表格，可以搭配第三方 UI 组件库使用。
+        </vxe-tip>
+        <pre>
+          <pre-code
+            language="shell"
             :content="`
-              import Vue from 'vue'
-              // ...
-
-              // 完整导入 UI 组件库
-              import VxeUI from 'vxe-pc-ui'
-              import 'vxe-pc-ui/lib/style.css'
-              // ...
-
-              // 完整导入 表格库
-              import VxeUITable from 'vxe-table'
-              import 'vxe-table/lib/style.css'
-              // ...
-
-              Vue.use(VxeUI)
-              Vue.use(VxeUITable)
-              //...
+            npm install ${ tableCDNLib }
+            # 或者
+            yarn add ${ tableCDNLib }
+            # 或者
+            pnpm add ${ tableCDNLib }
             `">
+          </pre-code>
+          <pre-code
+            language="javascript"
+            content="
+            // ...
+            import VxeUITable from 'vxe-table'
+            import 'vxe-table/lib/style.css'
+            // ...
+
+            Vue.use(VxeUITable)
+            // ...">
           </pre-code>
         </pre>
       </template>
--- v4/src/views/start/install/CodeExample.vue
+++ v4/src/views/start/install/CodeExample.vue
@@ -6,14 +6,40 @@
           <div>可以配合 <vxe-link  href="https://webpack.js.org/" target="_blank">webpack</vxe-link>、<vxe-link  href="https://vitejs.dev/" target="_blank">vite</vxe-link> 等打包工具配合使用。</div>
           <div>依赖库： <vxe-link  href="https://cn.vuejs.org/v2/guide/components-slots.html#%E5%8A%A8%E6%80%81%E6%8F%92%E6%A7%BD%E5%90%8D" target="_blank">vue 3.2+</vxe-link></div>
         </vxe-tip>
-        <vxe-tip status="error" title="需要注意！！！">
-          <div>v4.0 可以直接升级 v4.6（如果是使用老版本记得锁定版本号）</div>
-          <div>v4.6 不能直接升级 v4.7（组件代码基本不用改，变动的是安装方式、全局的主题变量）</div>
-        </vxe-tip>
       </template>
 
       <template #use>
-        <div>完整安装，可以直接使用完整功能。</div>
+        <vxe-tip status="success" title="方式1.完整安装表格和配套 UI 库">
+          搭配强大的 UI 库可以使用全功能组件
+        </vxe-tip>
+        <pre>
+          <pre-code
+            language="shell"
+            :content="`
+            npm install ${uiCDNLib } ${ tableCDNLib }
+            # 或者
+            yarn add ${uiCDNLib } ${ tableCDNLib }
+            # 或者
+            pnpm add ${uiCDNLib } ${ tableCDNLib }
+            `">
+          </pre-code>
+          <pre-code
+            language="javascript"
+            content="
+            // ...
+            import VxeUI from 'vxe-pc-ui'
+            import 'vxe-pc-ui/lib/style.css'
+            import VxeUITable from 'vxe-table'
+            import 'vxe-table/lib/style.css'
+            // ...
+
+            createApp(App).use(VxeUI).use(VxeUITable).mount('#app')
+            // ...">
+          </pre-code>
+        </pre>
+        <vxe-tip status="success" title="方式2.安装纯表格">
+          仅可使用表格，可以搭配第三方 UI 组件库使用。
+        </vxe-tip>
         <pre>
           <pre-code
             language="shell"
@@ -21,17 +47,14 @@
             npm install ${ tableCDNLib }
             # 或者
             yarn add ${ tableCDNLib }
+            # 或者
+            pnpm add ${ tableCDNLib }
             `">
           </pre-code>
           <pre-code
             language="javascript"
             content="
-            // 完整导入 UI 组件库
-            import VxeUI from 'vxe-pc-ui'
-            import 'vxe-pc-ui/lib/style.css'
             // ...
-
-            // 完整导入 表格库
             import VxeUITable from 'vxe-table'
             import 'vxe-table/lib/style.css'
             // ...
@@ -90,5 +113,6 @@ import { computed } from 'vue'
 import { useAppStore } from '@/store/app'
 
 const appStore = useAppStore()
+const uiCDNLib = computed(() => appStore.uiCDNLib)
 const tableCDNLib = computed(() => appStore.tableCDNLib)
 </script>
