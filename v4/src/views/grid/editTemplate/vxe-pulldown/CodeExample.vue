d91422f71ecf8bca31e2af9fa910ca2f3e5bdb4f
xuliangzhan <xlz26296x@163.com>
2024-10-23 18:21:59 +0800

    update docs

--- v4/src/views/grid/editTemplate/vxe-pulldown/CodeExample.vue
+++ v4/src/views/grid/editTemplate/vxe-pulldown/CodeExample.vue
@@ -7,6 +7,9 @@
         <vxe-tip status="primary" title="下拉容器">
           查看 <ApiLink name="pulldown" redirect/> 文档 API
         </vxe-tip>
+        <vxe-tip status="error">
+          需要注意，示例中的组件是全局安装的，非全局需自行导入对应的组件
+        </vxe-tip>
       </template>
     </CodeLight>
   </div>
