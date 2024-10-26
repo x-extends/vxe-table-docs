f9729af89946480cc2a41044feca04eeefd7670a
xuliangzhan <xlz26296x@163.com>
2024-10-26 20:43:55 +0800

    update docs

--- v3.8/src/views/start/StartTheme.vue
+++ v3.8/src/views/start/StartTheme.vue
@@ -1,19 +1,14 @@
 <template>
   <div>
     <h2>{{ $t('app.aside.nav.theme') }}</h2>
-    <p class="tip">第一种方式：创建 scss 文件（<a class="link" href="https://github.com/x-extends/vxe-table/blob/master/styles/variable.scss" target="_blank">查看所有变量</a>），并引入所有样式</p>
+    <p class="tip">全局引入，创建 scss 文件（<a class="link" href="https://github.com/x-extends/vxe-table/blob/v3.8/styles/variable.scss" target="_blank">查看所有变量</a>）</p>
     <pre>
       <pre-code class="scss">
-        // 修改scss变量
-        $vxe-font-size: 14px;
-        $vxe-font-color: #666;
-        $vxe-primary-color: #409eff;
-        $vxe-table-font-color: $vxe-font-color;
-        $vxe-table-border-color: #e8eaec;
-        $vxe-table-border-radius: 4px;
-        // ...
-
-        @import 'vxe-table/styles/index.scss';
+        @use 'vxe-table/styles/variable.scss' with (
+          $vxe-font-color: #999999,
+          $vxe-primary-color: #ff0000
+        );
+        @use 'vxe-table/styles/modules.scss';
       </pre-code>
     </pre>
   </div>
--- v4.6/src/views/start/StartTheme.vue
+++ v4.6/src/views/start/StartTheme.vue
@@ -14,27 +14,28 @@
       </pre-code>
     </pre>
 
-    <!-- <h2>重写样式，覆盖默认主题样式</h2>
-    <p class="tip">修改 css 变量（<a class="link" href="https://github.com/x-extends/vxe-table/blob/master/styles/cssvar.scss" target="_blank">查看所有变量</a>），并引入所有样式</p>
+    <h2>方式二：重写 css 变量</h2>
+    <p class="tip">全局引入，创建 css 文件（<a class="link" href="https://github.com/x-extends/vxe-table/blob/v4.6/styles/theme/default.scss" target="_blank">查看所有变量</a>）</p>
     <pre>
       <pre-code class="scss">
-        @import 'vxe-table/lib/style.css';
-
         // 给 html 加 class，例如 black-theme，然后修改css变量
         html.black-theme {
-          --vxe-font-color: #98989E;
-          --vxe-primary-color: #2C7ECF;
-          --vxe-icon-background-color: #98989E;
-          --vxe-table-font-color: #98989E;
-          --vxe-table-resizable-color: #95969a;
-          --vxe-table-header-background-color: #28282A;
-          --vxe-table-body-background-color: #151518;
-          --vxe-table-background-color: #4a5663;
-          --vxe-table-border-width: 1px;
-          --vxe-table-border-color: #37373A;
-          // ...
+          --vxe-ui-font-color: #999999,
+          --vxe-ui-font-primary-color: #ff0000;
         }
       </pre-code>
-    </pre> -->
+    </pre>
+
+    <h2>方式三：重写 scss 变量</h2>
+    <p class="tip">全局引入，创建 scss 文件（<a class="link" href="https://github.com/x-extends/vxe-table/blob/v4.6/styles/variable.scss" target="_blank">查看所有变量</a>）</p>
+    <pre>
+      <pre-code class="scss">
+        @use 'vxe-table/styles/variable.scss' with (
+          $vxe-font-color: #999999,
+          $vxe-primary-color: #ff0000
+        );
+        @use 'vxe-table/styles/modules.scss';
+      </pre-code>
+    </pre>
   </div>
 </template>
