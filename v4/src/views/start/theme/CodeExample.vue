7dc47265aed041182b3620d449ebd2348acb9129
xuliangzhan <xlz26296x@163.com>
2024-11-03 22:23:10 +0800

    update docs

--- v4/src/views/start/theme/CodeExample.vue
+++ v4/src/views/start/theme/CodeExample.vue
@@ -2,7 +2,7 @@
   <div>
     <CodeLight>
       <template #tip>
-        <vxe-tip status="primary" title="内置主题">
+        <vxe-tip status="primary" title="切换内置主题">
           默认支持 light 和 dark 两种主题样式，通过调用 VxeUI.setTheme('light') 方式修改组件主题样式
         </vxe-tip>
       </template>
@@ -24,7 +24,32 @@
 
     <CodeLight>
       <template #tip>
-        <vxe-tip status="primary" title="自定义 css 变量">
+        <vxe-tip status="primary" title="内置主题样式修改">
+          通过修内置主题 css 变量（<vxe-link href="https://gitee.com/xuliangzhan/vxe-table/blob/main/styles/theme/light.scss" status="primary" target="_blank">查看全部变量</vxe-link>）方式修改内置主题的样式：
+        </vxe-tip>
+      </template>
+
+      <template #use>
+        <pre>
+          <pre-code class="css">
+            // 默认主题
+            [data-vxe-ui-theme="light"] {
+              --vxe-ui-font-color: #606266;
+              --vxe-ui-font-primary-color: #409eff;
+            }
+            // 暗黑主题
+            [data-vxe-ui-theme="dark"] {
+              --vxe-ui-font-color: #a0a3a7;
+              --vxe-ui-font-primary-color: #409eff;
+            }
+          </pre-code>
+        </pre>
+      </template>
+    </CodeLight>
+
+    <CodeLight>
+      <template #tip>
+        <vxe-tip status="primary" title="自定义主题的样式修改">
           通过修改 css 变量（<vxe-link href="https://gitee.com/xuliangzhan/vxe-table/blob/master/styles/theme/light.scss" status="primary" target="_blank">查看全部变量</vxe-link>）方式修改组件样式，给 html 加个 class="my-theme"，示例：
         </vxe-tip>
       </template>
@@ -32,7 +57,7 @@
       <template #use>
         <pre>
           <pre-code class="css">
-            // 给 html 加 class，例如 my-theme，然后修改css变量
+            // 给 html 加对应的 class 就可以生效，例如 my-theme，然后修改css变量
             html.my-theme {
               --vxe-ui-font-color: #999999,
               --vxe-ui-font-primary-color: #ff0000;
@@ -44,8 +69,8 @@
 
     <CodeLight>
       <template #tip>
-        <vxe-tip status="primary" title="自定义 scss 变量">
-          通过修改 scss 变量（<vxe-link href="https://gitee.com/xuliangzhan/vxe-table/blob/master/styles/variable.scss" status="primary" target="_blank">查看全部变量</vxe-link>）方式修改组件样式，示例：
+        <vxe-tip status="primary" title="自定义全局 scss 变量">
+          通过修改 scss 变量（<vxe-link href="https://gitee.com/xuliangzhan/vxe-table/blob/master/styles/variable.scss" status="primary" target="_blank">查看全部变量</vxe-link>）方式修改全局组件样式，示例：
         </vxe-tip>
       </template>
 
