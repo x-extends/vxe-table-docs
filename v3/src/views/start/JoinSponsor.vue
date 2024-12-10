c9340033243205db0d40f9c1b11321f6c8c7e0b1
xuliangzhan <xlz26296x@163.com>
2024-12-10 14:51:25 +0800

    update docs

--- v3/src/views/start/JoinSponsor.vue
+++ v3/src/views/start/JoinSponsor.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
     <p class="tip">{{ pageTitle }} 是 MIT 开源的，使用完全免费。为了使项目能更好的持续维护，您可以通过成为赞助商方式来支持作者。</p>
-    <div class="content" style="padding-top: 50px">
-      <div class="sponsor-title">* 赞助（1000元/30天，1800元/60天）</div>
+    <div v-if="supportOptions.length" class="content" style="padding-top: 50px">
+      <div class="sponsor-title">
+        <span>* 赞助</span>
+        <span>（</span>
+        <span v-for="(item, index) in supportOptions" :key="index">
+          <span>{{ item.price }}元/{{ item.days }}天</span>
+          <span>，</span>
+        </span>
+        <span>）</span>
+      </div>
       <div class="sponsor-desc">当前站点所有页面的右侧展示位（图片尺寸200*100）</div>
       <div class="sponsor-desc">联系邮箱：x_extends@163.com</div>
     </div>
@@ -12,12 +20,29 @@
 <script lang="ts">
 import Vue from 'vue'
 import { mapState } from 'vuex'
+import axios from 'axios'
 
 export default Vue.extend({
+  data () {
+    const supportOptions: {
+      days: number
+      price: number
+    }[] = []
+
+    return {
+      supportOptions
+    }
+  },
   computed: {
     ...mapState([
+      'siteBaseUrl',
       'pageTitle'
     ])
+  },
+  created () {
+    axios.get(`${this.siteBaseUrl}/component-api/${process.env.VUE_APP_PACKAGE_NAME}-sponsor-config.json?v=${process.env.VUE_APP_DATE_NOW}`).then(res => {
+      this.supportOptions = res.data ? res.data.sponsors : []
+    })
   }
 })
 </script>
--- v4/src/views/start/JoinSponsor.vue
+++ v4/src/views/start/JoinSponsor.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
-    <p class="tip">{{ appStore.pageTitle }} 是 MIT 开源的，使用完全免费。为了使项目能更好的持续维护，您可以通过成为赞助商方式来支持作者。</p>
-    <div class="content" style="padding-top: 50px">
-      <div class="sponsor-title">* 赞助（1000元/30天，1800元/60天）</div>
+    <p class="tip">{{ pageTitle }} 是 MIT 开源的，使用完全免费。为了使项目能更好的持续维护，您可以通过成为赞助商方式来支持作者。</p>
+    <div v-if="supportOptions.length" class="content" style="padding-top: 50px">
+      <div class="sponsor-title">
+        <span>* 赞助</span>
+        <span>（</span>
+        <span v-for="(item, index) in supportOptions" :key="index">
+          <span>{{ item.price }}元/{{ item.days }}天</span>
+          <span>，</span>
+        </span>
+        <span>）</span>
+      </div>
       <div class="sponsor-desc">当前站点所有页面的右侧展示位（图片尺寸200*100）</div>
       <div class="sponsor-desc">联系邮箱：x_extends@163.com</div>
     </div>
@@ -10,9 +18,22 @@
 </template>
 
 <script lang="ts" setup>
+import { ref, computed } from 'vue'
 import { useAppStore } from '@/store/app'
+import axios from 'axios'
 
 const appStore = useAppStore()
+const siteBaseUrl = computed(() => appStore.siteBaseUrl)
+const pageTitle = computed(() => appStore.pageTitle)
+
+const supportOptions = ref<{
+  days: number
+  price: number
+}[]>([])
+
+axios.get(`${siteBaseUrl.value}/component-api/${process.env.VUE_APP_PACKAGE_NAME}-sponsor-config.json?v=${process.env.VUE_APP_DATE_NOW}`).then(res => {
+  supportOptions.value = res.data ? res.data.sponsors : []
+})
 </script>
 
 <style lang="scss" scoped>
