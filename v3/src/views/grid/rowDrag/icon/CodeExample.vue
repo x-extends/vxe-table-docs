eb15e687c1ae7a8b606887f7db524978c0937620
xuliangzhan <xu_liangzhan@163.com>
2025-01-09 16:19:48 +0800

    update docs

--- v3/src/views/grid/rowDrag/icon/CodeExample.vue
+++ v3/src/views/grid/rowDrag/icon/CodeExample.vue
@@ -7,5 +7,11 @@
         </vxe-tip>
       </template>
     </CodeLight>
+
+    <CodeLight path="grid/rowDrag/icon/Demo2">
+      <template #tip>
+        <vxe-tip status="primary">自定义插槽模板</vxe-tip>
+      </template>
+    </CodeLight>
   </div>
 </template>
--- v4/src/views/grid/colDrag/icon/CodeExample.vue
+++ v4/src/views/grid/colDrag/icon/CodeExample.vue
@@ -7,5 +7,11 @@
         </vxe-tip>
       </template>
     </CodeLight>
+
+    <CodeLight path="grid/colDrag/icon/Demo2">
+      <template #tip>
+        <vxe-tip status="primary">自定义插槽模板</vxe-tip>
+      </template>
+    </CodeLight>
   </div>
 </template>
