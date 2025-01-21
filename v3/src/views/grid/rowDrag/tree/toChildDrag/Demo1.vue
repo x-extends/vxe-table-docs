52218e1d411dd21658c4f9ca287ce64f274ade13
xuliangzhan <xu_liangzhan@163.com>
2025-01-21 16:42:30 +0800

    update docs

--- v3/src/views/grid/rowDrag/tree/toChildDrag/Demo1.vue
+++ v3/src/views/grid/rowDrag/tree/toChildDrag/Demo1.vue
@@ -9,8 +9,8 @@ import Vue from 'vue'
 import type { VxeGridProps } from 'vxe-table'
 
 interface RowVO {
-  id: number
-  parentId: number | null
+  code: number
+  parentCode: number | null
   name: string
   type: string
   size: number
@@ -33,8 +33,8 @@ export default Vue.extend({
       },
       treeConfig: {
         transform: true,
-        rowField: 'id',
-        parentField: 'parentId'
+        rowField: 'code',
+        parentField: 'parentCode'
       },
       columns: [
         { type: 'seq', width: 70 },
@@ -44,24 +44,24 @@ export default Vue.extend({
         { field: 'date', title: 'Date' }
       ],
       data: [
-        { id: 10000, parentId: null, name: 'Test1', type: 'mp3', size: 1024, date: '2020-08-01' },
-        { id: 10050, parentId: null, name: 'Test2', type: 'mp4', size: 0, date: '2021-04-01' },
-        { id: 24300, parentId: 10050, name: 'Test3', type: 'avi', size: 1024, date: '2020-03-01' },
-        { id: 20045, parentId: 24300, name: 'Test4', type: 'html', size: 600, date: '2021-04-01' },
-        { id: 10053, parentId: 24300, name: 'Test5', type: 'avi', size: 0, date: '2021-04-01' },
-        { id: 24330, parentId: 10053, name: 'Test6', type: 'txt', size: 25, date: '2021-10-01' },
-        { id: 21011, parentId: 10053, name: 'Test7', type: 'pdf', size: 512, date: '2020-01-01' },
-        { id: 22200, parentId: 10053, name: 'Test8', type: 'js', size: 1024, date: '2021-06-01' },
-        { id: 23666, parentId: null, name: 'Test9', type: 'xlsx', size: 2048, date: '2020-11-01' },
-        { id: 23677, parentId: 23666, name: 'Test10', type: 'js', size: 1024, date: '2021-06-01' },
-        { id: 23671, parentId: 23677, name: 'Test11', type: 'js', size: 1024, date: '2021-06-01' },
-        { id: 23672, parentId: 23677, name: 'Test12', type: 'js', size: 1024, date: '2021-06-01' },
-        { id: 23688, parentId: 23666, name: 'Test13', type: 'js', size: 1024, date: '2021-06-01' },
-        { id: 23681, parentId: 23688, name: 'Test14', type: 'js', size: 1024, date: '2021-06-01' },
-        { id: 23682, parentId: 23688, name: 'Test15', type: 'js', size: 1024, date: '2021-06-01' },
-        { id: 24555, parentId: null, name: 'Test16', type: 'avi', size: 224, date: '2020-10-01' },
-        { id: 24566, parentId: 24555, name: 'Test17', type: 'js', size: 1024, date: '2021-06-01' },
-        { id: 24577, parentId: 24555, name: 'Test18', type: 'js', size: 1024, date: '2021-06-01' }
+        { code: 10000, parentCode: null, name: 'Test1', type: 'mp3', size: 1024, date: '2020-08-01' },
+        { code: 10050, parentCode: null, name: 'Test2', type: 'mp4', size: 0, date: '2021-04-01' },
+        { code: 24300, parentCode: 10050, name: 'Test3', type: 'avi', size: 1024, date: '2020-03-01' },
+        { code: 20045, parentCode: 24300, name: 'Test4', type: 'html', size: 600, date: '2021-04-01' },
+        { code: 10053, parentCode: 24300, name: 'Test5', type: 'avi', size: 0, date: '2021-04-01' },
+        { code: 24330, parentCode: 10053, name: 'Test6', type: 'txt', size: 25, date: '2021-10-01' },
+        { code: 21011, parentCode: 10053, name: 'Test7', type: 'pdf', size: 512, date: '2020-01-01' },
+        { code: 22200, parentCode: 10053, name: 'Test8', type: 'js', size: 1024, date: '2021-06-01' },
+        { code: 23666, parentCode: null, name: 'Test9', type: 'xlsx', size: 2048, date: '2020-11-01' },
+        { code: 23677, parentCode: 23666, name: 'Test10', type: 'js', size: 1024, date: '2021-06-01' },
+        { code: 23671, parentCode: 23677, name: 'Test11', type: 'js', size: 1024, date: '2021-06-01' },
+        { code: 23672, parentCode: 23677, name: 'Test12', type: 'js', size: 1024, date: '2021-06-01' },
+        { code: 23688, parentCode: 23666, name: 'Test13', type: 'js', size: 1024, date: '2021-06-01' },
+        { code: 23681, parentCode: 23688, name: 'Test14', type: 'js', size: 1024, date: '2021-06-01' },
+        { code: 23682, parentCode: 23688, name: 'Test15', type: 'js', size: 1024, date: '2021-06-01' },
+        { code: 24555, parentCode: null, name: 'Test16', type: 'avi', size: 224, date: '2020-10-01' },
+        { code: 24566, parentCode: 24555, name: 'Test17', type: 'js', size: 1024, date: '2021-06-01' },
+        { code: 24577, parentCode: 24555, name: 'Test18', type: 'js', size: 1024, date: '2021-06-01' }
       ]
     }
 
