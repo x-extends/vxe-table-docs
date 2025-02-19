23aba05ea454d23191bd3c932c1fcf25330afcf2
xuliangzhan <xu_liangzhan@163.com>
2025-02-19 11:07:58 +0800

    update docs

--- v4/src/views/demo/product/CodeExample.vue
+++ v4/src/views/demo/product/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="demo/product/Demo1">
+    <CodeLight path="demo/product/Demo1" previewPath="/resource/docsImg/table_demo_product.gif">
       <template #tip>
         <vxe-tip status="primary" title="商品列表"></vxe-tip>
       </template>
