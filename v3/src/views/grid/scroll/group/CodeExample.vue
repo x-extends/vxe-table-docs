8ee88511cc9fd042548f633c59d8d6f951dc6dd6
xuliangzhan <xu_liangzhan@163.com>
2025-06-18 18:15:10 +0800

    update docs

--- v3/src/views/grid/scroll/group/CodeExample.vue
+++ v3/src/views/grid/scroll/group/CodeExample.vue
@@ -13,6 +13,9 @@
         <vxe-tip status="primary">
           横向虚拟滚动
         </vxe-tip>
+        <vxe-tip status="error">
+          当使用分组表头后，表头是不支持虚拟滚动的，应避免使用大量列头
+        </vxe-tip>
       </template>
     </CodeLight>
   </div>
