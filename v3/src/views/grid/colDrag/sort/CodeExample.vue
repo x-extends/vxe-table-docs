e0988b759ad8994053b53caf87310527a13dbf74
xuliangzhan <xu_liangzhan@163.com>
2025-06-19 15:28:59 +0800

    update docs

--- v3/src/views/grid/colDrag/sort/CodeExample.vue
+++ v3/src/views/grid/colDrag/sort/CodeExample.vue
@@ -5,6 +5,9 @@
         <vxe-tip status="primary" title="列拖拽">
           通过 <ApiLink name="grid" prop="column-config"/>.<ApiLink name="grid" prop="drag"/> 启用列拖拽排序功能
         </vxe-tip>
+        <vxe-tip status="error">
+          通过 <ApiLink name="grid" prop="column-drag-config"/>.<ApiLink name="grid" prop="animation"/>=false 来关闭拖拽动画可以大幅提高流畅度
+        </vxe-tip>
       </template>
 
       <template #describe>
--- v4/src/views/grid/rowDrag/sort/CodeExample.vue
+++ v4/src/views/grid/rowDrag/sort/CodeExample.vue
@@ -5,6 +5,9 @@
         <vxe-tip status="primary" title="行拖拽">
           通过 <ApiLink name="grid" prop="row-config"/>.<ApiLink name="grid" prop="drag"/> 和列设置 <ApiLink name="column" prop="drag-sort"/> 启用行拖拽排序功能
         </vxe-tip>
+        <vxe-tip status="error">
+          通过 <ApiLink name="grid" prop="row-drag-config"/>.<ApiLink name="grid" prop="animation"/>=false 来关闭拖拽动画可以大幅提高流畅度
+        </vxe-tip>
       </template>
 
       <template #describe>
