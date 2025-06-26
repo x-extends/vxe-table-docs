74093ea755a3583e76bac9a66387ac932135c3bc
xuliangzhan <xu_liangzhan@163.com>
2025-06-26 16:10:52 +0800

    update docs

--- v4/src/views/demo/compare/CodeExample.vue
+++ v4/src/views/demo/compare/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="demo/compare/Demo1">
+    <CodeLight path="demo/compare/Demo1" previewPath="/resource/docsImg/table_demo_compare.gif">
       <template #tip>
         <vxe-tip status="primary" title="产品对比表"></vxe-tip>
       </template>
