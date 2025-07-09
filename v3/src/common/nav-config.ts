3f1bb3720066f17bd48b5c34caf47598b8900cc5
xuliangzhan <xu_liangzhan@163.com>
2025-07-09 10:37:52 +0800

    update docs

--- v3/src/common/nav-config.ts
+++ v3/src/common/nav-config.ts
@@ -1557,6 +1557,7 @@ export const navConfigList: NavVO[] = [
       { i18nKey: 'app.aside.menu.demoRealTime', routerLink: { name: 'DemoRealTimeReview' } },
       { i18nKey: 'app.aside.menu.demoTree', routerLink: { name: 'DemoTreePreview' } },
       { i18nKey: 'app.aside.menu.demoCrud', routerLink: { name: 'DemoCrudPreview' } },
+      { title: 'plugins.base.nav.demoGroupSummary', isEnterprise: true, routerLink: { name: 'EnterprisePreview', params: { previewCode: 'extend_pivot_table_table_custom_agg_drag_values' }, query: { previewPath: '%2FextendPivotTable%2Fgrid%2Fcustom%2FaggDragValues' } } },
       { i18nKey: 'plugins.base.nav.demoClip', isEnterprise: true, routerLink: { name: 'EnterprisePreview', params: { previewCode: 'extend_cell_area_demo_clip' }, query: { previewPath: '%2Fdemo%2Fclip' } } },
       { i18nKey: 'plugins.base.nav.demoChart', isEnterprise: true, routerLink: { name: 'EnterprisePreview', params: { previewCode: 'extend_cell_area_demo_chart' }, query: { previewPath: '%2Fdemo%2Fchart' } } },
       { i18nKey: 'plugins.base.nav.demoExcel', isEnterprise: true, routerLink: { name: 'EnterprisePreview', params: { previewCode: 'extend_cell_area_demo_excel' }, query: { previewPath: '%2Fdemo%2Fexcel' } } }
