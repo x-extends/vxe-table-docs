4a386881b23a75713f330abace2d37d1e6053039
xuliangzhan <xu_liangzhan@163.com>
2025-07-23 17:32:49 +0800

    update docs

--- v3/src/views/grid/import/advanced/CodeExample.vue
+++ v3/src/views/grid/import/advanced/CodeExample.vue
@@ -5,6 +5,9 @@
         <vxe-tip status="primary" title="高级导入">
           必须设置参数 <ApiLink name="grid" prop="import-config"/> 才能启用高级导入功能
         </vxe-tip>
+        <vxe-tip status="error">
+          需要注意导入是不支持分组列头和表尾数据的
+        </vxe-tip>
       </template>
     </CodeLight>
   </div>
