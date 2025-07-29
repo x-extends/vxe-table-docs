21000ca3852e255981b7f2253a048967f7d05b91
xuliangzhan <xu_liangzhan@163.com>
2025-07-29 10:31:07 +0800

    update docs

--- v4/src/views/grid/base/visible/CodeExample.vue
+++ v4/src/views/grid/base/visible/CodeExample.vue
@@ -2,7 +2,7 @@
   <div>
     <CodeLight path="grid/base/visible/Demo1">
       <template #tip>
-        <vxe-tip status="primary" title="列可视">通过 <ApiLink name="column" prop="visible"/> 设置列是否显示 </vxe-tip>
+        <vxe-tip status="primary" title="隐藏列">通过 <ApiLink name="column" prop="visible"/> 设置列是否显示 </vxe-tip>
       </template>
     </CodeLight>
   </div>
