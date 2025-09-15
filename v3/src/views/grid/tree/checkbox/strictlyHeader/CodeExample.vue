7af6195473b0b44d799f434f5808ba907dc61ecc
xuliangzhan <xu_liangzhan@163.com>
2025-09-15 17:37:29 +0800

    update docs

--- v3/src/views/grid/tree/checkbox/strictlyHeader/CodeExample.vue
+++ v3/src/views/grid/tree/checkbox/strictlyHeader/CodeExample.vue
@@ -5,6 +5,9 @@
         <vxe-tip status="primary" title="父子不关联与全选">
           通过 showHeader 启用列头全选
         </vxe-tip>
+        <vxe-tip status="error">
+          需要注意取消父子关联后，全选状态只作用于第一级，子孙级是不会被更新状态的
+        </vxe-tip>
       </template>
     </CodeLight>
   </div>
