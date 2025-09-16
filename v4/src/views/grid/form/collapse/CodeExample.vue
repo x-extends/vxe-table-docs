8a9ff6ae1109cdbd79dc7c963b597c4c2ae0806f
xuliangzhan <xu_liangzhan@163.com>
2025-09-16 18:09:18 +0800

    update docs

--- v4/src/views/grid/form/collapse/CodeExample.vue
+++ v4/src/views/grid/form/collapse/CodeExample.vue
@@ -3,7 +3,7 @@
     <CodeLight path="grid/form/collapse/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="折叠表单">
-          通过给表单项设置 <ApiLink name="form" prop="folding"/> 属性为默认不显示，设置 <ApiLink name="form" prop="collapse-node"/> 为折叠按钮的操作项
+          通过给表单项设置 <ApiLink name="form" prop="folding"/> 属性默认是不显示的，设置 <ApiLink name="form" prop="collapse-node"/> 为折叠按钮的操作项
         </vxe-tip>
       </template>
     </CodeLight>
