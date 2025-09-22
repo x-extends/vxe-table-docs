0bafb07dfba7d5d3a065725b5eccac8d0df77504
xuliangzhan <xu_liangzhan@163.com>
2025-09-22 14:40:31 +0800

    update docs

--- v3/src/views/start/permission/CodeExample.vue
+++ v3/src/views/start/permission/CodeExample.vue
@@ -12,9 +12,9 @@
 
       <template #use>
         <pre>
-          <pre-code language="javascript">
-            import { VxeUI } from 'vxe-table'
-            // VxeUI 是 Vxe 库通用全局实例（也可以使用旧别名 VXETable）
+          <pre-code class="javascript">
+            import { VxeUI } from 'vxe-pc-ui'
+            // VxeUI 是 Vxe 库通用全局实例
 
             VxeUI.setConfig({
               // 全局权限码控制方法
@@ -33,20 +33,34 @@
                 }
               }
             })
-
-            if (VxeUI.permission.checkVisible('xxx')) {
-              // 判断是否有权限
-            }
           </pre-code>
-          <div>在组件中指定 permissionCode 权限码，当无权限时会被自动隐藏</div>
-          <pre-code language="html">
+          <div>在组件中使用，指定 permissionCode 权限码，当无权限时会被自动隐藏</div>
+          <pre-code class="html">
             &lt;vxe-button permission-code="xxx"&gt;按钮&lt;/vxe-button&gt;
             &lt;vxe-link permission-code="xxx"&gt;链接&lt;/vxe-link&gt;
           </pre-code>
-          <div>如果多个权限码，支持分隔符：|（或）</div>
-          <pre-code language="html">
+          <div>在组件中使用，如果多个权限码，支持分隔符：|（或）</div>
+          <pre-code class="html">
             &lt;vxe-button permission-code="aaa|bbb"&gt;按钮&lt;/vxe-button&gt;
             &lt;vxe-link permission-code="aaa|bbb"&gt;链接&lt;/vxe-link&gt;
+          </pre-code>
+          <div>如果在 js 中使用</div>
+          <pre-code class="javascript">
+            if (VxeUI.permission.checkVisible('xxx')) {
+              // 判断是否有可视权限
+            }
+
+            if (VxeUI.permission.checkDisable('xxx')) {
+              // 判断是否被禁用
+            }
+
+            const permissionInfo = VxeUI.permission.getCheckInfo('xxx')
+            if (permissionInfo.visible) {
+              // 判断是否有可视权限
+            }
+            if (permissionInfo.disabled) {
+              // 判断是否被禁用
+            }
           </pre-code>
         </pre>
       </template>
--- v4/src/views/start/permission/CodeExample.vue
+++ v4/src/views/start/permission/CodeExample.vue
@@ -3,17 +3,18 @@
     <CodeLight>
       <template #tip>
         <vxe-tip status="primary" title="组件权限控制">
-          <div>通过权限控制，您可以轻松对各类组件进行精确的控制。例如：表格按钮等...</div>
+          <div>通过权限控制，您可以轻松对各类组件进行精确的控制。例如：菜单、表格按钮等...</div>
         </vxe-tip>
-        <vxe-tip status="success">
-          支持权限码的组件：vxe-button、vxe-button-group
+        <vxe-tip status="error">
+          只对支持权限码的组件有效
         </vxe-tip>
       </template>
 
       <template #use>
         <pre>
-          <pre-code language="javascript">
-            import { VxeUI } from 'vxe-table'
+          <pre-code class="javascript">
+            import { VxeUI } from 'vxe-pc-ui'
+            // VxeUI 是 Vxe 库通用全局实例
 
             VxeUI.setConfig({
               // 全局权限码控制方法
@@ -32,20 +33,34 @@
                 }
               }
             })
-
-            if (VxeUI.permission.checkVisible('xxx')) {
-              // 判断是否有权限
-            }
           </pre-code>
-          <div>在组件中指定 permissionCode 权限码，当无权限时会被自动隐藏</div>
-          <pre-code language="html">
+          <div>在组件中使用，指定 permissionCode 权限码，当无权限时会被自动隐藏</div>
+          <pre-code class="html">
             &lt;vxe-button permission-code="xxx"&gt;按钮&lt;/vxe-button&gt;
             &lt;vxe-link permission-code="xxx"&gt;链接&lt;/vxe-link&gt;
           </pre-code>
-          <div>如果多个权限码，支持分隔符：|（或）</div>
-          <pre-code language="html">
+          <div>在组件中使用，如果多个权限码，支持分隔符：|（或）</div>
+          <pre-code class="html">
             &lt;vxe-button permission-code="aaa|bbb"&gt;按钮&lt;/vxe-button&gt;
             &lt;vxe-link permission-code="aaa|bbb"&gt;链接&lt;/vxe-link&gt;
+          </pre-code>
+          <div>如果在 js 中使用</div>
+          <pre-code class="javascript">
+            if (VxeUI.permission.checkVisible('xxx')) {
+              // 判断是否有可视权限
+            }
+
+            if (VxeUI.permission.checkDisable('xxx')) {
+              // 判断是否被禁用
+            }
+
+            const permissionInfo = VxeUI.permission.getCheckInfo('xxx')
+            if (permissionInfo.visible) {
+              // 判断是否有可视权限
+            }
+            if (permissionInfo.disabled) {
+              // 判断是否被禁用
+            }
           </pre-code>
         </pre>
       </template>
