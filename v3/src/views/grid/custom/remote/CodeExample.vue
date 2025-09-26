c96e3689c8be666d808a62fdaba16a8a55f7a431
xuliangzhan <xu_liangzhan@163.com>
2025-09-26 18:00:47 +0800

    update docs

--- v3/src/views/grid/custom/remote/CodeExample.vue
+++ v3/src/views/grid/custom/remote/CodeExample.vue
@@ -4,7 +4,7 @@
       <template #tip>
         <vxe-tip status="primary">通过自定义 updateStore 方法可以将数据状态保存到本地或者服务端，从而实现用户个性化列设置服务端保存 </vxe-tip>
         <vxe-tip status="error">
-          用户个性化记忆功能是根据表格 id 进行保存的，必须确保表格 <ApiLink name="grid" prop="id"/> 是整个全局唯一的，同时每一列必须设置唯一 <ApiLink name="grid" prop="field"/> 属性,如果是分组列头也一样,设置一个唯一字段值就行
+          用户个性化记忆功能是根据表格 id 进行保存的，必须确保表格 <ApiLink name="grid" prop="id"/> 是整个全局唯一的，同时每一列必须设置唯一 <ApiLink name="grid" prop="field"/> 属性,如果是分组列头也一样，设置一个唯一字段值就行
         </vxe-tip>
       </template>
     </CodeLight>
--- v4/src/views/grid/custom/remoteFull/CodeExample.vue
+++ v4/src/views/grid/custom/remoteFull/CodeExample.vue
@@ -4,7 +4,7 @@
       <template #tip>
         <vxe-tip status="primary">通过自定义 updateStore 和 restoreStore 方法可以将服务端数据状态异步还原到本地，从而实现用户个性化列设置服务端还原 </vxe-tip>
         <vxe-tip status="error">
-          用户个性化记忆功能是根据表格 id 进行保存的，必须确保表格 <ApiLink name="grid" prop="id"/> 是整个全局唯一的，同时每一列必须设置唯一 <ApiLink name="grid" prop="field"/> 属性,如果是分组列头也一样,设置一个唯一字段值就行
+          用户个性化记忆功能是根据表格 id 进行保存的，必须确保表格 <ApiLink name="grid" prop="id"/> 是整个全局唯一的，同时每一列必须设置唯一 <ApiLink name="grid" prop="field"/> 属性,如果是分组列头也一样，设置一个唯一字段值就行
         </vxe-tip>
       </template>
     </CodeLight>
