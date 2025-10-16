3faf44e7a9c9ac78cb7aa3becf1a24a7a9ebad64
xuliangzhan <xu_liangzhan@163.com>
2025-10-16 16:14:00 +0800

    update docs

--- v3/src/views/grid/other/treeSubCols/CodeExample.vue
+++ v3/src/views/grid/other/treeSubCols/CodeExample.vue
@@ -2,8 +2,8 @@
   <div>
     <CodeLight path="grid/other/treeSubCols/Demo1">
       <template #tip>
-        <vxe-tip status="primary" title="树形实现子集自定义表头">
-          当某些场景，一个带层级的数据结构，同时每个子集都有独立的表头
+        <vxe-tip status="primary" title="树形实现子节点自定义表头">
+          当某些场景，一个带层级的数据结构，同时每个子节点都有独立的表头
         </vxe-tip>
       </template>
     </CodeLight>
