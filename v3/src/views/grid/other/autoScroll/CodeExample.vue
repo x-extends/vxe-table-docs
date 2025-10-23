239d3e5234153e4e7b9d3e903ac17ec6f65f5fa5
xuliangzhan <xu_liangzhan@163.com>
2025-10-23 09:10:02 +0800

    update docs

--- v3/src/views/grid/other/autoScroll/CodeExample.vue
+++ v3/src/views/grid/other/autoScroll/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="grid/other/autoScroll/Demo1">
+    <CodeLight path="grid/other/autoScroll/Demo1" previewPath="/resource/docsImg/table_other_auto_scroll.gif">
       <template #tip>
         <vxe-tip status="primary" title="实现数据自动滚动">
           还可以通过 <ApiLink name="grid" prop="scrollbar-config"/>.<ApiLink name="grid" prop="x"/>.<ApiLink name="grid" prop="visible"/> 与 <ApiLink name="grid" prop="scrollbar-config"/>.<ApiLink name="grid" prop="y"/>.<ApiLink name="grid" prop="visible"/> 设置去掉滚动条
