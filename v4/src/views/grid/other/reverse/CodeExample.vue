31c2e89e1271d0934cff34827b90e6bb9fa86f39
xuliangzhan <xu_liangzhan@163.com>
2025-10-23 11:04:59 +0800

    update docs

--- v4/src/views/grid/other/reverse/CodeExample.vue
+++ v4/src/views/grid/other/reverse/CodeExample.vue
@@ -2,8 +2,8 @@
   <div>
     <CodeLight path="grid/other/reverse/Demo1">
       <template #tip>
-        <vxe-tip status="primary" title="反转表格">
-          反转表格，只需要将 columns 和 data 数据进行反转就可以实现
+        <vxe-tip status="primary" title="模拟反转表格">
+          模拟反转表格，只需要将 columns 和 data 数据进行反转就可以实现，仅仅只是展示反转
         </vxe-tip>
       </template>
     </CodeLight>
@@ -11,7 +11,7 @@
     <CodeLight path="grid/other/reverse/Demo2">
       <template #tip>
         <vxe-tip status="primary">
-          实现列与行反转
+          实现列与行反转，仅仅只是展示反转
         </vxe-tip>
       </template>
     </CodeLight>
