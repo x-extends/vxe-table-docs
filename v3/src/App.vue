e4c3b6ed340c4040c2f4a213fc6dc7c39dcd94d1
xuliangzhan <xu_liangzhan@163.com>
2025-10-24 15:10:55 +0800

    update docs

--- v3/src/App.vue
+++ v3/src/App.vue
@@ -5,9 +5,10 @@
   </VxeLayoutContainer>
 </template>
 
-<script lang="ts">
+<script lang="tsx">
 import Vue from 'vue'
 import { mapState, mapMutations } from 'vuex'
+import { VxeUI } from 'vxe-table'
 import axios from 'axios'
 
 export default Vue.extend({
@@ -31,6 +32,31 @@ export default Vue.extend({
     axios.get(`${this.siteBaseUrl}/component-api/vxe-version.json?v=${process.env.VUE_APP_DATE_NOW}`).then(res => {
       this.setVersionConfig(res.data)
     })
+
+    const voteKey = 'VXE_VOTE_2025_3'
+    if (!localStorage.getItem(voteKey)) {
+      VxeUI.modal.confirm({
+        width: 500,
+        title: 'vxe-table 正在参加 Gitee 2025 最受欢迎的开源软件投票活动',
+        confirmButtonText: '去投票',
+        cancelButtonText: '忽略，不再弹出',
+        slots: {
+          default () {
+            return <div>
+              <div>如果该项目对您有帮助，快来投票！</div>
+              <div><vxe-link status="primary" href="https://gitee.com/activity/2025opensource?ident=IX0B8Q" target="_blank">https://gitee.com/activity/2025opensource?ident=IX0B8Q</vxe-link></div>
+            </div>
+          }
+        }
+      }).then((type) => {
+        if (type === 'confirm') {
+          localStorage.setItem(voteKey, '1')
+          open('https://gitee.com/activity/2025opensource?ident=IX0B8Q')
+        } else if (type === 'cancel') {
+          localStorage.setItem(voteKey, '1')
+        }
+      })
+    }
   }
 })
 </script>
--- v4/src/App.vue
+++ v4/src/App.vue
@@ -5,9 +5,10 @@
   </VxeLayoutContainer>
 </template>
 
-<script lang="ts" setup>
+<script lang="tsx" setup>
 import { computed } from 'vue'
 import { useAppStore } from '@/store/app'
+import {VxeUI} from 'vxe-table'
 import axios from 'axios'
 
 const appStore = useAppStore()
@@ -21,4 +22,29 @@ axios.get(`${siteBaseUrl.value}/component-api/system-config.json?v=${import.meta
 axios.get(`${siteBaseUrl.value}/component-api/vxe-version.json?v=${import.meta.env.VITE_APP_DATE_NOW}`).then(res => {
   appStore.setVersionConfig(res.data)
 })
+
+const voteKey = 'VXE_VOTE_2025_4'
+if (!localStorage.getItem(voteKey)) {
+  VxeUI.modal.confirm({
+    width: 500,
+    title: 'vxe-table 正在参加 Gitee 2025 最受欢迎的开源软件投票活动',
+    confirmButtonText: '去投票',
+    cancelButtonText: '忽略，不再弹出',
+    slots: {
+      default () {
+        return <div>
+          <div>如果该项目对您有帮助，快来投票！</div>
+          <div><vxe-link status="primary" href="https://gitee.com/activity/2025opensource?ident=IX0B8Q" target="_blank">https://gitee.com/activity/2025opensource?ident=IX0B8Q</vxe-link></div>
+        </div>
+      }
+    }
+  }).then((type) => {
+    if (type === 'confirm') {
+      localStorage.setItem(voteKey, '1')
+      open('https://gitee.com/activity/2025opensource?ident=IX0B8Q')
+    } else if (type === 'cancel') {
+      localStorage.setItem(voteKey, '1')
+    }
+  })
+}
 </script>
