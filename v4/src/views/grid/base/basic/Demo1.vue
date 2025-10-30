7a6dd553272b923885e63782e1fab222036a8e56
xuliangzhan <xu_liangzhan@163.com>
2025-10-30 10:34:35 +0800

    update docs

--- v4/src/views/grid/base/basic/Demo1.vue
+++ v4/src/views/grid/base/basic/Demo1.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <vxe-grid v-bind="gridOptions"></vxe-grid>
+    <vxe-grid :columns="tableColumn" :data="tableData"></vxe-grid>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { reactive } from 'vue'
-import type { VxeGridProps } from 'vxe-table'
+import { ref } from 'vue'
+import type { VxeGridPropTypes } from 'vxe-table'
 
 interface RowVO {
   id: number
@@ -17,18 +17,17 @@ interface RowVO {
   address: string
 }
 
-const gridOptions = reactive<VxeGridProps<RowVO>>({
-  columns: [
-    { type: 'seq', width: 70 },
-    { field: 'name', title: 'Name' },
-    { field: 'sex', title: 'Sex' },
-    { field: 'age', title: 'Age' }
-  ],
-  data: [
-    { id: 10001, name: 'Test1', role: 'Develop', sex: 'Man', age: 28, address: 'test abc' },
-    { id: 10002, name: 'Test2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou' },
-    { id: 10003, name: 'Test3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
-    { id: 10004, name: 'Test4', role: 'Designer', sex: 'Women', age: 24, address: 'Shanghai' }
-  ]
-})
+const tableColumn = ref<VxeGridPropTypes.Columns>([
+  { type: 'seq', width: 70 },
+  { field: 'name', title: 'Name' },
+  { field: 'sex', title: 'Sex' },
+  { field: 'age', title: 'Age' }
+])
+
+const tableData = ref<RowVO[]>([
+  { id: 10001, name: 'Test1', role: 'Develop', sex: 'Man', age: 28, address: 'test abc' },
+  { id: 10002, name: 'Test2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou' },
+  { id: 10003, name: 'Test3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
+  { id: 10004, name: 'Test4', role: 'Designer', sex: 'Women', age: 24, address: 'Shanghai' }
+])
 </script>
