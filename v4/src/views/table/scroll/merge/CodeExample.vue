c4678b26f8cba0ff13a7d9ec5f3187aa5643b8d9
xuliangzhan <xu_liangzhan@163.com>
2025-10-31 14:47:21 +0800

    update docs

--- v4/src/views/table/scroll/merge/CodeExample.vue
+++ v4/src/views/table/scroll/merge/CodeExample.vue
@@ -6,7 +6,7 @@
           虚拟渲染与单元格合并，可以通过设置参数 <ApiLink name="table" prop="merge-cells"/> 或调用函数 <ApiLink name="table" prop="setMergeCells"/>、<ApiLink name="table" prop="setMergeCells"/> 来控制单元格的临时合并状态<br>
         </vxe-tip>
         <vxe-tip status="error">
-          <div>合并的性能是非常低的，当所有行都被合并为一行时就等同于关闭虚拟滚动。</div>
+          <div>合并的性能是非常低的，当所有行都被合并为一行时就等同于关闭虚拟滚动。应该避免合并太多行的使用场景</div>
         </vxe-tip>
       </template>
     </CodeLight>
