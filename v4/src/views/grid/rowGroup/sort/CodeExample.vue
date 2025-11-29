ef740147f3597aa426b0a91cf715b24e6ac7a225
xuliangzhan <xu_liangzhan@163.com>
2025-11-29 09:54:55 +0800

    update docs

--- v4/src/views/grid/rowGroup/sort/CodeExample.vue
+++ v4/src/views/grid/rowGroup/sort/CodeExample.vue
@@ -3,7 +3,7 @@
     <CodeLight path="grid/rowGroup/sort/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="排序">
-          使用分组排序
+          使用分组排序,当没有定义分组合计时，只排序子项
         </vxe-tip>
       </template>
     </CodeLight>
@@ -11,7 +11,7 @@
     <CodeLight path="grid/rowGroup/sort/Demo2">
       <template #tip>
         <vxe-tip status="primary">
-          同时排序分组合计
+          当定义分组合计时，同时排序分组合计
         </vxe-tip>
       </template>
     </CodeLight>
