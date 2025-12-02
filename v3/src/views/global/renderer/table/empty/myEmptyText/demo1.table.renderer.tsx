1433ade3716640f4227fbbc5920d42041c90a631
xuliangzhan <xu_liangzhan@163.com>
2025-12-02 20:10:51 +0800

    update docs

--- v3/src/views/global/renderer/table/empty/myEmptyText/demo1.table.renderer.tsx
+++ v3/src/views/global/renderer/table/empty/myEmptyText/demo1.table.renderer.tsx
@@ -5,7 +5,7 @@ import { VxeUI } from 'vxe-pc-ui'
 // 创建一个空内容渲染
 VxeUI.renderer.add('MyTableEmptyText', {
   // 空内容模板
-  renderEmpty () {
+  renderTableEmpty () {
     return <span>
       <img src="https://vxeui.com/resource/img/546.gif"/>
       <p>亲，没有更多数据了！</p>
