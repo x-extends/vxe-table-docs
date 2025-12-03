97a53587a1c5200e779df5075d4ad2e71214789f
xuliangzhan <xu_liangzhan@163.com>
2025-12-03 16:11:38 +0800

    update docs

--- v4/src/views/grid/scroll/merge/CodeExample.vue
+++ v4/src/views/grid/scroll/merge/CodeExample.vue
@@ -10,7 +10,7 @@
         </vxe-tip>
         <vxe-tip status="warning" title="小提示">
           <div>如果使用 <ApiLink name="grid" prop="merge-cells"/> 方式则是全功能的，支持虚拟滚动；</div>
-          <div>如果是 <ApiLink name="grid" prop="span-method"/> 方式的虚拟滚动支持部分功能，存在功能限制：如果进行跨行合并，则不支持纵向虚拟滚动；如果进行跨列合并，则不支持横向虚拟滚动</div>
+          <div>如果使用 <ApiLink name="grid" prop="span-method"/> 方式的虚拟滚动支持部分功能，存在功能限制：如果进行跨行合并，则不支持纵向虚拟滚动；如果进行跨列合并，则不支持横向虚拟滚动</div>
         </vxe-tip>
       </template>
     </CodeLight>
