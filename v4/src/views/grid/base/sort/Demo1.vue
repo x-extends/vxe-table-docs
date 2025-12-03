50f83697be8d0578dbfbe7935cdccfd73d4bccf1
xuliangzhan <xu_liangzhan@163.com>
2025-12-03 11:25:16 +0800

    update docs

--- v4/src/views/grid/base/sort/Demo1.vue
+++ v4/src/views/grid/base/sort/Demo1.vue
@@ -20,6 +20,7 @@ interface RowVO {
   age: number
   num: string
   num2: string
+  num3: string
   address: string
 }
 
@@ -38,17 +39,18 @@ const gridOptions = reactive<VxeGridProps<RowVO>>({
     { field: 'age', title: 'Age', sortable: true },
     { field: 'num', title: '字符串', sortable: true, sortType: 'string' },
     { field: 'num2', title: '数值', sortable: true, sortType: 'number' },
+    { field: 'num3', title: '百分比', sortable: true, sortType: 'number' },
     { field: 'address', title: 'Address', sortable: true }
   ],
   data: [
-    { id: 10001, name: 'Test1', role: 'Develop', sex: 'Man', age: 28, num: '3.8', num2: '3.8', address: 'test abc' },
-    { id: 10002, name: 'Test2', role: 'Test', sex: 'Women', age: 22, num: '511', num2: '511', address: 'Guangzhou' },
-    { id: 10003, name: 'Test3', role: 'PM', sex: 'Man', age: 32, num: '12.8', num2: '12.8', address: 'Shanghai' },
-    { id: 10004, name: 'Test4', role: 'Designer', sex: 'Women', age: 23, num: '103', num2: '103', address: 'test abc' },
-    { id: 10005, name: 'Test5', role: 'Develop', sex: 'Women', age: 30, num: '56', num2: '56', address: 'Shanghai' },
-    { id: 10006, name: 'Test6', role: 'Designer', sex: 'Women', age: 21, num: '49', num2: '49', address: 'test abc' },
-    { id: 10007, name: 'Test7', role: 'Test', sex: 'Man', age: 29, num: '400.9', num2: '400.9', address: 'test abc' },
-    { id: 10008, name: 'Test8', role: 'Develop', sex: 'Man', age: 35, num: '5000', num2: '5000', address: 'test abc' }
+    { id: 10001, name: 'Test1', role: 'Develop', sex: 'Man', age: 28, num: '3.8', num2: '3.8', num3: '9%', address: 'test abc' },
+    { id: 10002, name: 'Test2', role: 'Test', sex: 'Women', age: 22, num: '511', num2: '511', num3: '98.66%', address: 'Guangzhou' },
+    { id: 10003, name: 'Test3', role: 'PM', sex: 'Man', age: 32, num: '12.8', num2: '12.8', num3: '25%', address: 'Shanghai' },
+    { id: 10004, name: 'Test4', role: 'Designer', sex: 'Women', age: 23, num: '103', num2: '103', num3: '98%', address: 'test abc' },
+    { id: 10005, name: 'Test5', role: 'Develop', sex: 'Women', age: 30, num: '56', num2: '56', num3: '99.71%', address: 'Shanghai' },
+    { id: 10006, name: 'Test6', role: 'Designer', sex: 'Women', age: 21, num: '49', num2: '49', num3: '30.1%', address: 'test abc' },
+    { id: 10007, name: 'Test7', role: 'Test', sex: 'Man', age: 29, num: '400.9', num2: '400.9', num3: '30.5%', address: 'test abc' },
+    { id: 10008, name: 'Test8', role: 'Develop', sex: 'Man', age: 35, num: '5000', num2: '5000', num3: '100%', address: 'test abc' }
   ]
 })
 
