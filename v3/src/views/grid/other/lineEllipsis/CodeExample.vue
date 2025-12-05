e75218f7692531aae5388a491465b3d68649b43d
xuliangzhan <xu_liangzhan@163.com>
2025-12-05 22:03:50 +0800

    update docs

--- v3/src/views/grid/other/lineEllipsis/CodeExample.vue
+++ v3/src/views/grid/other/lineEllipsis/CodeExample.vue
@@ -5,6 +5,9 @@
         <vxe-tip status="primary" title="实现单元格多行文本溢出省略">
           实现 3 行文本溢出隐藏，支持自定义行数
         </vxe-tip>
+        <vxe-tip status="error">
+          该方式不支持自动显示溢出文本提示，可以配合 <ApiLink name="grid" prop="tooltip-config"/>.<ApiLink name="grid" prop="showAll"/> 使用
+        </vxe-tip>
       </template>
     </CodeLight>
   </div>
