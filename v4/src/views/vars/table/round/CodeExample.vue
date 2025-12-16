f37a0501905afaa1c44bd2e8027e51ec988aff73
xuliangzhan <xu_liangzhan@163.com>
2025-12-16 14:05:40 +0800

    update docs

--- v4/src/views/vars/table/round/CodeExample.vue
+++ v4/src/views/vars/table/round/CodeExample.vue
@@ -7,5 +7,13 @@
         </vxe-tip>
       </template>
     </CodeLight>
+
+    <CodeLight path="vars/table/round/Demo2">
+      <template #tip>
+        <vxe-tip status="primary">
+          滚动条是不支持圆角的，可以自行通过样式去修改
+        </vxe-tip>
+      </template>
+    </CodeLight>
   </div>
 </template>
--- v4/src/views/vars/table/round/Demo2.vue
+++ v4/src/views/vars/table/round/Demo2.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <vxe-radio-group v-model="borderColor">
-      <vxe-radio-button label="" content="默认"></vxe-radio-button>
+      <vxe-radio-button label="4px" content="默认"></vxe-radio-button>
       <vxe-radio-button label="#ff0000" content="红色"></vxe-radio-button>
       <vxe-radio-button label="#0000ff" content="蓝色"></vxe-radio-button>
     </vxe-radio-group>
     <vxe-radio-group v-model="borderRadius">
-      <vxe-radio-button label="" content="4px"></vxe-radio-button>
+      <vxe-radio-button label="4px" content="4px"></vxe-radio-button>
       <vxe-radio-button label="8px" content="8px"></vxe-radio-button>
       <vxe-radio-button label="16px" content="16px"></vxe-radio-button>
       <vxe-radio-button label="32px" content="32px"></vxe-radio-button>
@@ -40,6 +40,7 @@ const borderRadius = ref('8px')
 
 const gridOptions = reactive<VxeGridProps<RowVO>>({
   border: true,
+  height: 300,
   showFooter: true,
   round: true,
   columns: [
@@ -52,7 +53,9 @@ const gridOptions = reactive<VxeGridProps<RowVO>>({
     { id: 10001, name: 'Test1', role: 'Develop', sex: 'Man', age: 28, address: 'test abc' },
     { id: 10002, name: 'Test2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou' },
     { id: 10003, name: 'Test3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
-    { id: 10004, name: 'Test4', role: 'Designer', sex: 'Women', age: 24, address: 'Shanghai' }
+    { id: 10004, name: 'Test4', role: 'Designer', sex: 'Women', age: 24, address: 'Shanghai' },
+    { id: 10005, name: 'Test5', role: 'Test', sex: 'Man', age: 39, address: 'Guangzhou' },
+    { id: 10006, name: 'Test6', role: 'Develop', sex: 'Women', age: 29, address: 'test abc' }
   ],
   footerData: [
     { seq: '合计', name: '777', sex: '333', age: '111' }
