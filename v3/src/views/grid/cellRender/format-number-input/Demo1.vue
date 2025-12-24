c7517bb9df28fda7e81a63696406279d60e96460
xuliangzhan <xu_liangzhan@163.com>
2025-12-24 16:21:30 +0800

    update docs

--- v3/src/views/grid/cellRender/format-number-input/Demo1.vue
+++ v3/src/views/grid/cellRender/format-number-input/Demo1.vue
@@ -19,6 +19,7 @@ interface RowVO {
   num2: number | null
   num3: number | null
   num4: number | null
+  num5: number | null
 }
 
 export default Vue.extend({
@@ -31,14 +32,15 @@ export default Vue.extend({
         { field: 'name', title: 'Name' },
         { field: 'num1', title: '数值', cellRender: { name: 'FormatNumberInput' } },
         { field: 'num2', title: '数值（负数标红）', cellRender: { name: 'FormatNumberInput', showNegativeStatus: true } },
+        { field: 'num5', title: '小数', cellRender: { name: 'FormatNumberInput', props: { type: 'float', digits: 3 } } },
         { field: 'num3', title: '货币', align: 'right', cellRender: { name: 'FormatNumberInput', props: { type: 'amount', align: 'right', showCurrency: true } } },
         { field: 'num4', title: '货币（负数标红）', align: 'right', cellRender: { name: 'FormatNumberInput', showNegativeStatus: true, props: { type: 'amount', align: 'right', showCurrency: true } } }
       ],
       data: [
-        { id: 10001, name: 'Test1', role: 'Develop', sex: 'Man', age: 28, address: 'test abc', num1: 10000000, num2: 10000000, num3: 10000000, num4: 10000000 },
-        { id: 10002, name: 'Test2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou', num1: -20000, num2: -20000, num3: -20000, num4: -20000 },
-        { id: 10003, name: 'Test3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai', num1: null, num2: null, num3: null, num4: null },
-        { id: 10004, name: 'Test4', role: 'Designer', sex: 'Women', age: 24, address: 'Shanghai', num1: 0, num2: 0, num3: 0, num4: 0 }
+        { id: 10001, name: 'Test1', role: 'Develop', sex: 'Man', age: 28, address: 'test abc', num1: 10000000, num2: 10000000, num3: 10000000, num4: 10000000, num5: 1000 },
+        { id: 10002, name: 'Test2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou', num1: -20000, num2: -20000, num3: -20000, num4: -20000, num5: 0.1 },
+        { id: 10003, name: 'Test3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai', num1: null, num2: null, num3: null, num4: null, num5: 500.2 },
+        { id: 10004, name: 'Test4', role: 'Designer', sex: 'Women', age: 24, address: 'Shanghai', num1: 0, num2: 0, num3: 0, num4: 0, num5: 90.6847 }
       ]
     }
 
--- v4/src/views/grid/cellRender/format-number-input/CodeExample.vue
+++ v4/src/views/grid/cellRender/format-number-input/CodeExample.vue
@@ -3,7 +3,7 @@
     <CodeLight path="grid/cellRender/format-number-input/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="负数自动标红">
-          FormatNumberInput 格式化数字, { name: 'FormatNumberInput', showNegativeStatus: boolean, props: {} }
+          FormatNumberInput 格式化数字, { name: 'FormatNumberInput', showNegativeStatus: boolean, <ApiLink name="number-input" prop="props"/> }
         </vxe-tip>
         <vxe-tip status="error">
           需要注意只要特定的控件才支持自动显示负数标红
