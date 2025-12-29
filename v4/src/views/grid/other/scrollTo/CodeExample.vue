b9eeffc9e44c9ea3dcaae598e2919fe2ce243e87
xuliangzhan <xu_liangzhan@163.com>
2025-12-29 17:32:35 +0800

    update docs

--- v4/src/views/grid/other/scrollTo/CodeExample.vue
+++ v4/src/views/grid/other/scrollTo/CodeExample.vue
@@ -3,7 +3,7 @@
     <CodeLight path="grid/other/scrollTo/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="定位行、定位列">
-          通过调用 <ApiLink name="grid" prop="scrollRow"/> 或 <ApiLink name="grid" prop="scrollColumn"/> 滚动到对应的单元格。该功能对于很多场景都非常有用，调用定位可以使单元格始终保持在表格的可视区内。
+          通过调用 <ApiLink name="grid" prop="scrollToRow"/> 或 <ApiLink name="grid" prop="scrollToColumn"/> 滚动到对应的单元格。该功能对于很多场景都非常有用，调用定位可以使单元格始终保持在表格的可视区内。
         </vxe-tip>
       </template>
     </CodeLight>
