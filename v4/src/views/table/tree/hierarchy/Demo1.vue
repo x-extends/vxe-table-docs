d22105439113a6b5d4911d0af5e30045895ee3f1
xuliangzhan <xu_liangzhan@163.com>
2025-12-31 16:22:23 +0800

    update docs

--- v4/src/views/table/tree/hierarchy/Demo1.vue
+++ v4/src/views/table/tree/hierarchy/Demo1.vue
@@ -18,88 +18,88 @@ import { ref, reactive } from 'vue'
 import { VxeTablePropTypes } from 'vxe-table'
 
 interface RowVO {
-  id: number
+  keyId: number
   name: string
   type: string
   size: number
   date: string
-  children?: RowVO[]
+  childList?: RowVO[]
 }
 
 const tableData = ref<RowVO[]>([
-  { id: 10000, name: 'Test1', type: 'mp3', size: 1024, date: '2020-08-01' },
+  { keyId: 10000, name: 'Test1', type: 'mp3', size: 1024, date: '2020-08-01' },
   {
-    id: 10050,
+    keyId: 10050,
     name: 'Test2',
     type: 'mp4',
     size: 0,
     date: '2021-04-01',
-    children: [
+    childList: [
       {
-        id: 24300,
+        keyId: 24300,
         name: 'Test3',
         type: 'avi',
         size: 1024,
         date: '2020-03-01',
-        children: [
-          { id: 20045, name: 'Test4', type: 'html', size: 600, date: '2021-04-01' },
+        childList: [
+          { keyId: 20045, name: 'Test4', type: 'html', size: 600, date: '2021-04-01' },
           {
-            id: 10053,
+            keyId: 10053,
             name: 'Test5',
             type: 'avi',
             size: 0,
             date: '2021-04-01',
-            children: [
-              { id: 24330, name: 'Test6', type: 'txt', size: 25, date: '2021-10-01' },
-              { id: 21011, name: 'Test7', type: 'pdf', size: 512, date: '2020-01-01' },
-              { id: 22200, name: 'Test8', type: 'js', size: 1024, date: '2021-06-01' }]
+            childList: [
+              { keyId: 24330, name: 'Test6', type: 'txt', size: 25, date: '2021-10-01' },
+              { keyId: 21011, name: 'Test7', type: 'pdf', size: 512, date: '2020-01-01' },
+              { keyId: 22200, name: 'Test8', type: 'js', size: 1024, date: '2021-06-01' }]
           }
         ]
       }]
   },
   {
-    id: 23666,
+    keyId: 23666,
     name: 'Test9',
     type: 'xlsx',
     size: 2048,
     date: '2020-11-01',
-    children: [
+    childList: [
       {
-        id: 23677,
+        keyId: 23677,
         name: 'Test10',
         type: 'js',
         size: 1024,
         date: '2021-06-01',
-        children: [
-          { id: 23671, name: 'Test11', type: 'js', size: 1024, date: '2021-06-01' },
-          { id: 23672, name: 'Test12', type: 'js', size: 1024, date: '2021-06-01' }]
+        childList: [
+          { keyId: 23671, name: 'Test11', type: 'js', size: 1024, date: '2021-06-01' },
+          { keyId: 23672, name: 'Test12', type: 'js', size: 1024, date: '2021-06-01' }]
       },
       {
-        id: 23688,
+        keyId: 23688,
         name: 'Test13',
         type: 'js',
         size: 1024,
         date: '2021-06-01',
-        children: [
-          { id: 23681, name: 'Test14', type: 'js', size: 1024, date: '2021-06-01' },
-          { id: 23682, name: 'Test15', type: 'js', size: 1024, date: '2021-06-01' }]
+        childList: [
+          { keyId: 23681, name: 'Test14', type: 'js', size: 1024, date: '2021-06-01' },
+          { keyId: 23682, name: 'Test15', type: 'js', size: 1024, date: '2021-06-01' }]
       }
     ]
   },
   {
-    id: 24555,
+    keyId: 24555,
     name: 'Test16',
     type: 'avi',
     size: 224,
     date: '2020-10-01',
-    children: [
-      { id: 24566, name: 'Test17', type: 'js', size: 1024, date: '2021-06-01' },
-      { id: 24577, name: 'Test18', type: 'js', size: 1024, date: '2021-06-01' }]
+    childList: [
+      { keyId: 24566, name: 'Test17', type: 'js', size: 1024, date: '2021-06-01' },
+      { keyId: 24577, name: 'Test18', type: 'js', size: 1024, date: '2021-06-01' }]
   }
 ])
 
 const treeConfig = reactive<VxeTablePropTypes.TreeConfig<RowVO>>({
-  rowField: 'id',
-  childrenField: 'children'
+  rowField: 'keyId',
+  childrenField: 'childList'
 })
 </script>
