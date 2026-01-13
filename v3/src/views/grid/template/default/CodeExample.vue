fc91fcb8a1d214b3039306f21d93921c3549cb4b
xuliangzhan <xu_liangzhan@163.com>
2026-01-13 19:41:40 +0800

    update docs

--- v3/src/views/grid/template/default/CodeExample.vue
+++ v3/src/views/grid/template/default/CodeExample.vue
@@ -6,7 +6,7 @@
           单元格模板，使用插槽 <ApiLink name="column" prop="default"/> 来自定义模板
         </vxe-tip>
         <vxe-tip status="success">
-          <div>渲染性能：提前转换好数据给data=[{...}]（最优） > <ApiLink name="column" prop="formatter"/>格式化，field值发生变化时调用（较好） > <ApiLink name="column" prop="slots"/>自定义插槽模板，实时响应调用（差）</div>
+          <div>渲染性能：提前转换好数据给data=[{...}]（最优） > <ApiLink name="column" prop="formatter"/>格式化，field值发生变化时调用（优） > <ApiLink name="column" prop="slots"/>自定义插槽模板，实时响应调用（差）</div>
         </vxe-tip>
       </template>
     </CodeLight>
