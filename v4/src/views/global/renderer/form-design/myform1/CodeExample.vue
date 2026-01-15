b12fa128a6aa9d8d9b359f2fc13904cc5efc982c
xuliangzhan <xu_liangzhan@163.com>
2026-01-15 19:17:40 +0800

    update docs

--- v4/src/views/global/renderer/form-design/myform1/CodeExample.vue
+++ v4/src/views/global/renderer/form-design/myform1/CodeExample.vue
@@ -5,7 +5,7 @@
       :extraImports="['./data.ts', './DemoDesignMyForm1Setting.vue', './demo1.form-design.renderer.tsx']">
       <template #tip>
         <vxe-tip status="primary" title="自定义表单1">
-          通过 form-render 可以针对不同业务场景创建不同的表单，比如桌面端，移动端，小程序等，当然也可以一套表单适配多端
+          通过 <ApiLink name="form-design" prop="form-render"/> 可以针对不同业务场景创建不同的表单，比如桌面端，移动端，小程序等，当然也可以一套表单适配多端
         </vxe-tip>
       </template>
     </CodeLight>
