c7b2277012ddfaef10b7588fcfefb8a2cc99cfe1
xuliangzhan <xu_liangzhan@163.com>
2026-01-28 16:05:09 +0800

    update docs

--- v4/src/views/global/renderer/form-design/myform1/data.ts
+++
@@ -1,4 +0,0 @@
-// 定义自己的表单数据
-export interface DemoDesignMyForm1Data {
-  isLog: boolean
-}
--- v4/src/views/global/renderer/form-design/myform2/data.ts
+++
@@ -1,6 +0,0 @@
-// 定义自己的表单数据
-export interface DemoDesignMyForm2Data {
-  isPC: boolean,
-  isMobile: boolean
-  isApplet: boolean
-}
