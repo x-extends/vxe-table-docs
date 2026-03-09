6234fe3011868a34f5ff52b6cff2f0eab2d3d3e9
xuliangzhan <xu_liangzhan@163.com>
2026-03-09 16:55:03 +0800

    update docs

--- v4/src/views/table/rowGroup/count/Demo1.vue
+++ v4/src/views/table/rowGroup/count/Demo1.vue
@@ -8,7 +8,7 @@
       <vxe-column type="seq" width="70"></vxe-column>
       <vxe-column field="name" title="Name" min-width="300" row-group-node></vxe-column>
       <vxe-column field="role" title="Role"></vxe-column>
-      <vxe-column field="sex" title="Sex" :agg-func="true"></vxe-column>
+      <vxe-column field="num" title="Num" :agg-func="true"></vxe-column>
       <vxe-column field="age" title="Age" :agg-func="true"></vxe-column>
       <vxe-column field="address" title="Address"></vxe-column>
     </vxe-table>
