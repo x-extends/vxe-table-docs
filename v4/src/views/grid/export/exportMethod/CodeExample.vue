5a0694c36e83478534be7d8b59cbb104e0f0d42c
xuliangzhan <xu_liangzhan@163.com>
2026-05-09 18:15:25 +0800

    update docs

--- v4/src/views/grid/export/exportMethod/CodeExample.vue
+++ v4/src/views/grid/export/exportMethod/CodeExample.vue
@@ -3,10 +3,10 @@
     <CodeLight path="grid/export/exportMethod/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="格式化导出内容">
-          通过 <ApiLink name="column" prop="export-method"/> 可以格式化导出单元格的内容
+          通过 <ApiLink name="grid" prop="export-method"/> 可以格式化导出单元格的内容
         </vxe-tip>
         <vxe-tip status="error" title="小提示">
-          优先级 <ApiLink name="column" prop="export-method"/> > <ApiLink name="column" prop="formatter"/>，两者区别是一个会显示在单元格中，一个不会显示在单元格中
+          优先级 <ApiLink name="grid" prop="export-method"/> > <ApiLink name="grid" prop="formatter"/> > <ApiLink name="grid" prop="field"/>，两者区别是一个会显示在单元格中，一个不会显示在单元格中
         </vxe-tip>
       </template>
     </CodeLight>
@@ -14,7 +14,7 @@
     <CodeLight path="grid/export/exportMethod/Demo2">
       <template #tip>
         <vxe-tip status="primary">
-          通过 <ApiLink name="column" prop="formatter"/> 来格式化
+          通过 <ApiLink name="grid" prop="formatter"/> 来格式化
         </vxe-tip>
       </template>
     </CodeLight>
