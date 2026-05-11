cadac773daf886975163a11d57b40b905588e81b
xuliangzhan <xu_liangzhan@163.com>
2026-05-11 17:06:00 +0800

    update docs

--- v3/src/views/grid/merge/vnGroup/CodeExample.vue
+++ v3/src/views/grid/merge/vnGroup/CodeExample.vue
@@ -2,8 +2,8 @@
   <div>
     <CodeLight path="grid/merge/vnGroup/Demo1">
       <template #tip>
-        <vxe-tip status="primary" title="使用合并列来展示分类">
-          实现思路，插入空行，通过合并方式来展示分类的显示样式
+        <vxe-tip status="primary" title="分组报表效果">
+          实现思路，插入空行，通过合并方式来展示分组报表效果
         </vxe-tip>
       </template>
     </CodeLight>
