5323b76d24281e8d8832dface3cb170e39d012d9
xuliangzhan <xu_liangzhan@163.com>
2026-05-11 11:07:48 +0800

    update docs

--- v4/src/views/table/edit/footerImmediately/CodeExample.vue
+++ v4/src/views/table/edit/footerImmediately/CodeExample.vue
@@ -2,7 +2,7 @@
   <div>
     <CodeLight path="table/edit/footerImmediately/Demo1" previewPath="/resource/docsImg/table_edit_footer_Immediately.gif">
       <template #tip>
-        <vxe-tip status="primary" title="单击编辑">
+        <vxe-tip status="primary" title="实现表尾实时合计">
           <div>通过表尾来实现合计功能，数据发生变化时实时统计</div>
           <div>如果是内置的渲染器，可以设置 <ApiLink name="column" prop="immediate"/> 属性和相关事件去实时更新</div>
         </vxe-tip>
