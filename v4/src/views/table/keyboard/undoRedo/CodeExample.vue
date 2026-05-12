85f7b3b47c8f4f29357c573302cd1689b26afc9c
xuliangzhan <xu_liangzhan@163.com>
2026-05-12 10:26:57 +0800

    update docs

--- v4/src/views/table/keyboard/undoRedo/CodeExample.vue
+++ v4/src/views/table/keyboard/undoRedo/CodeExample.vue
@@ -8,8 +8,9 @@
         <vxe-tip status="error" title="支持场景">
           <div>✔️支持设置最大栈的历史步数</div>
           <div>✔️支持单元格编辑</div>
-          <div>⚠️不支持新增行</div>
-          <div>⚠️不支持删除行</div>
+          <div>✔️支持新增行</div>
+          <div>✔️支持删除行</div>
+          <div>⚠️其他功能不支持</div>
         </vxe-tip>
       </template>
 
