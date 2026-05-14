83bf785b1b154967be10dd8b663c3491504c04f3
xuliangzhan <xu_liangzhan@163.com>
2026-05-14 21:31:53 +0800

    update docs

--- v3/src/views/start/permission/CodeExample.vue
+++ v3/src/views/start/permission/CodeExample.vue
@@ -16,17 +16,17 @@
             import { VxeUI } from 'vxe-pc-ui'
             // VxeUI 是 Vxe 库通用全局实例
 
-            // 后端获取用户权限数据
-            const permissionMaps = {
-              'homePage': true,
-              'useManagePage': true,
-              'systemManagePage': true
+            // 后端获取用户路由权限数据
+            const userRouteMaps = {
+              'HomePage': true,
+              'UserManagePage': true,
+              'SystemManagePage': true
             }
 
             VxeUI.setConfig({
               // 全局权限码控制方法
               permissionMethod ({ code }) {
-                if (permissionMaps[code]) {
+                if (userRouteMaps[code]) {
                   return {
                     // 是否可视
                     visible: false,
@@ -43,32 +43,32 @@
           </pre-code>
           <div>在组件中使用，指定 permissionCode 权限码，当无权限时会被自动隐藏</div>
           <pre-code class="html">
-            &lt;vxe-button permission-code="homePage"&gt;按钮&lt;/vxe-button&gt;
-            &lt;vxe-link permission-code="homePage"&gt;链接&lt;/vxe-link&gt;
+            &lt;vxe-button permission-code="HomePage"&gt;按钮&lt;/vxe-button&gt;
+            &lt;vxe-link permission-code="HomePage"&gt;链接&lt;/vxe-link&gt;
           </pre-code>
           <div>在组件中使用，如果多个权限码，支持分隔符：|（或）</div>
           <pre-code class="html">
-            &lt;vxe-button permission-code="useManagePage|systemManagePage"&gt;按钮&lt;/vxe-button&gt;
-            &lt;vxe-link permission-code="homePage|useManagePage" href="#/home"&gt;链接&lt;/vxe-link&gt;
+            &lt;vxe-button permission-code="UserManagePage|SystemManagePage"&gt;按钮&lt;/vxe-button&gt;
+            &lt;vxe-link permission-code="HomePage|UserManagePage" href="#/home"&gt;链接&lt;/vxe-link&gt;
           </pre-code>
           <div>如果在 js 中使用</div>
           <pre-code class="javascript">
-            if (VxeUI.permission.checkVisible('homePage')) {
+            if (VxeUI.permission.checkVisible('HomePage')) {
               // 判断是否有可视权限
             }
-            if (VxeUI.permission.checkVisible('homePage|useManagePage')) {
+            if (VxeUI.permission.checkVisible('HomePage|UserManagePage')) {
               // 使用 | 隔开，任意一个为可视，则可视
             }
 
-            if (VxeUI.permission.checkDisable('homePage')) {
+            if (VxeUI.permission.checkDisable('HomePage')) {
               // 判断是否被禁用
             }
-            if (VxeUI.permission.checkDisable('homePage|useManagePage')) {
+            if (VxeUI.permission.checkDisable('HomePage|UserManagePage')) {
               // 使用 | 隔开，任意一个禁用，则禁用
             }
 
-            const permissionInfo = VxeUI.permission.getCheckInfo('homePage')
-            // const permissionInfo = VxeUI.permission.getCheckInfo('homePage|useManagePage')
+            const permissionInfo = VxeUI.permission.getCheckInfo('HomePage')
+            // const permissionInfo = VxeUI.permission.getCheckInfo('HomePage|UserManagePage')
             if (permissionInfo.visible) {
               // 判断是否有可视权限
             }
