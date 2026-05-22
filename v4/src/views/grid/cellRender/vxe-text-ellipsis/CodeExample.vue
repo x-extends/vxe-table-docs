6c13b06fa42b9333366edff008c40170167beb2e
xuliangzhan <xu_liangzhan@163.com>
2026-05-22 10:56:45 +0800

    update docs

--- v4/src/views/grid/cellRender/vxe-text-ellipsis/CodeExample.vue
+++ v4/src/views/grid/cellRender/vxe-text-ellipsis/CodeExample.vue
@@ -5,6 +5,9 @@
         <vxe-tip status="primary" title="多行文本溢出省略">
           查看 <ApiLink name="text-ellipsis" redirect/> 文档 API
         </vxe-tip>
+        <vxe-tip status="error" title="小提示">
+          但需要给单元格显示 tooltip 工具提示时，可以通过设置 <ApiLink name="grid" prop="tooltip-config"/>.<ApiLink name="grid" prop="showAll"/> 来开启
+        </vxe-tip>
       </template>
     </CodeLight>
   </div>
