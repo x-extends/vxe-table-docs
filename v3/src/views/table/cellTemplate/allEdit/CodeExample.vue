682976bec507fdfd8661607114d1afe3767c171d
xuliangzhan <xu_liangzhan@163.com>
2026-06-01 14:33:00 +0800

    update docs

--- v3/src/views/table/cellTemplate/allEdit/CodeExample.vue
+++ v3/src/views/table/cellTemplate/allEdit/CodeExample.vue
@@ -4,7 +4,7 @@
       <template #tip>
         <vxe-tip status="primary" title="实现全部单元格可编辑"></vxe-tip>
         <vxe-tip status="error">
-          适用数据量不大的特殊场景，渲染量越大就越卡顿
+          不支持内置校验，适用数据量不大的特殊场景，渲染量越大就越卡顿
         </vxe-tip>
       </template>
     </CodeLight>
@@ -15,7 +15,7 @@
           虚拟滚动，大数据量使用
         </vxe-tip>
         <vxe-tip status="error">
-          适用数据量不大的特殊场景，渲染量越大就越卡顿
+          不支持内置校验，适用数据量不大的特殊场景，渲染量越大就越卡顿
         </vxe-tip>
       </template>
     </CodeLight>
