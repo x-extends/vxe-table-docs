a669c35216343ac649f157bce1c88019288cfad6
xuliangzhan <xu_liangzhan@163.com>
2026-06-09 15:43:13 +0800

    update docs

--- v3/src/views/grid/custom/toCustomTmpl/CodeExample.vue
+++ v3/src/views/grid/custom/toCustomTmpl/CodeExample.vue
@@ -3,7 +3,7 @@
     <CodeLight path="grid/custom/toCustomTmpl/Demo1">
       <template #tip>
         <vxe-tip status="primary" title="自定义实现用户列设置">
-          通过调用方法 <ApiLink name="grid" prop="getCustomStoreData"/> 获取当前个性化数据，调用方法 <ApiLink name="grid" prop="setCustomStoreData"/> 可以重新初始化设置个性化数据，对于需要自定义实现或者多个性化模板时可能会用到
+          通过调用方法 <ApiLink name="grid" prop="getCustomStoreData"/> 获取当前个性化数据，调用方法 <ApiLink name="grid" prop="setCustomStoreData"/> 可以重新初始化设置个性化数据，对于需要自定义实现或者用户个性化模板多套时可能会用到
         </vxe-tip>
       </template>
     </CodeLight>
