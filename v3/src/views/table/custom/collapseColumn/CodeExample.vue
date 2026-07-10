e9c7900b7f1a6efba0f16236797ccd4738ab2635
xuliangzhan <xu_liangzhan@163.com>
2026-07-10 10:02:42 +0800

    update docs

--- v3/src/views/table/custom/collapseColumn/CodeExample.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <CodeLight path="table/custom/collapseColumn/Demo1">
-      <template #tip>
-        <vxe-tip status="primary">实现折叠列</vxe-tip>
-      </template>
-    </CodeLight>
-  </div>
-</template>
