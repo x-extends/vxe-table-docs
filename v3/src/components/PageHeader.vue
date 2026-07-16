6a725b48a58cc6cc9f3ed9d18c43c28bcfa8a293
xuliangzhan <xu_liangzhan@163.com>
2026-07-16 11:56:45 +0800

    update docs

--- v3/src/components/PageHeader.vue
+++ v3/src/components/PageHeader.vue
@@ -27,7 +27,7 @@
         <template #dropdown>
           <ul class="plugin-app-wrapper">
             <li v-for="(item, index) in pluginAppList" :key="index">
-              <vxe-link :href="`${tablePluginDocsUrl}/${item.uri}`" :content="$t(`shopping.apps.${item.code}`)"></vxe-link>
+              <vxe-link :href="isPluginDocs ? item.uri : `${tablePluginDocsUrl}/${item.uri}`" :content="$t(`shopping.apps.${item.code}`)"></vxe-link>
               <span v-if="item.isStore" class="enterprise">{{ $t('app.header.pluginStore') }}</span>
               <span v-else-if="item.isFree" class="free">{{ $t('app.header.freeVersion') }}</span>
               <span v-else-if="item.isEnterprise" class="enterprise">{{ $t('app.header.enterpriseVersion') }}</span>
