7e4ed1aefb5b01797725e8e1cde6b1ea9e67f52c
xuliangzhan <xu_liangzhan@163.com>
2026-07-22 22:34:16 +0800

    update docs

--- v3/src/views/table/cellTemplate/allEdit/CodeExample.vue
+++ v3/src/views/table/cellTemplate/allEdit/CodeExample.vue
@@ -14,9 +14,6 @@
         <vxe-tip status="primary">
           虚拟滚动，大数据量使用
         </vxe-tip>
-        <vxe-tip status="error">
-          不支持内置校验，适用数据量不大的特殊场景，渲染量越大就越卡顿
-        </vxe-tip>
       </template>
     </CodeLight>
   </div>
