536407d9830e8b6fd2ba9ae9c34deee159d92634
xuliangzhan <xu_liangzhan@163.com>
2026-07-28 15:23:01 +0800

    update docs

--- v4/src/views/table/base/overflow/CodeExample.vue
+++ v4/src/views/table/base/overflow/CodeExample.vue
@@ -9,8 +9,8 @@
           <ApiLink name="column" prop="tooltip"/> 当内容溢出时显示为省略号并用 tooltip 显示<br>
         </vxe-tip>
         <vxe-tip status="success" title="小提示">
-          <div>当设置 <ApiLink name="column" prop="show-overflow"/>=true 默认值由 <ApiLink name="table" prop="tooltip-config"/>.<ApiLink name="grid" prop="table"/> 设置</div>
-          <div>当设置 <ApiLink name="column" prop="show-header-overflow"/>=true 默认值由 <ApiLink name="table" prop="header-tooltip-config"/>.<ApiLink name="grid" prop="table"/> 设置</div>
+          <div>当设置 <ApiLink name="column" prop="show-overflow"/>=true 默认值由 <ApiLink name="table" prop="tooltip-config"/>.<ApiLink name="table" prop="mode"/> 设置</div>
+          <div>当设置 <ApiLink name="column" prop="show-header-overflow"/>=true 默认值由 <ApiLink name="table" prop="header-tooltip-config"/>.<ApiLink name="table" prop="mode"/> 设置</div>
           <div>当设置 <ApiLink name="column" prop="show-footer-overflow"/>=true 默认值由 <ApiLink name="table" prop="footer-tooltip-config"/>.<ApiLink name="table" prop="mode"/> 设置</div>
         </vxe-tip>
       </template>
