4dd8efccfc7c5d7f18e3d57809b6a9ff8f457701
xuliangzhan <xu_liangzhan@163.com>
2026-07-31 19:44:20 +0800

    update docs

--- v4/src/views/start/z-index/CodeExample.vue
+++ v4/src/views/start/z-index/CodeExample.vue
@@ -2,18 +2,18 @@
   <div>
     <CodeLight>
       <template #tip>
-        <vxe-tip status="primary" title="全局 z-index 调整">
+        <vxe-tip status="primary" title="动态 z-index 调整">
           <div>全局 z-index 默认起始是 999，如果项目的的 z-index 样式值过大时就需要跟随设置更大，避免被遮挡。可以通过以下全局设置，也可以去 <vxe-link status="primary" :router-link="{name: 'StartConfig'}">全局参数</vxe-link> 设置。</div>
           <div>使用 <vxe-link href="https://www.npmjs.com/package/dom-zindex" target="_blank" status="primary">dom-zindex</vxe-link> 来设置，更加方便快捷。</div>
         </vxe-tip>
         <vxe-tip status="success">
-          Vxe 组件使用 <vxe-link href="https://www.npmjs.com/package/dom-zindex" target="_blank" status="primary">dom-zindex</vxe-link> 规范共享 z-index，对于多个组件库共用的场景将非常有用
+          Vxe 组件使用 <vxe-link href="https://www.npmjs.com/package/dom-zindex" target="_blank" status="primary">dom-zindex</vxe-link> 规范共享 z-index，对于多个组件库共用的场景将非常有用；例如第三方组件的弹窗和表格，在弹窗显示是动态设置一下 domZIndex.setCurrent(xxx) 就可以达到互相兼容的效果。
         </vxe-tip>
       </template>
 
       <template #use>
         <pre>
-          <pre-code language="javascript">
+          <pre-code class="javascript">
             import domZIndex from 'dom-zindex'
 
             // 获取页面中最大的 z-index
