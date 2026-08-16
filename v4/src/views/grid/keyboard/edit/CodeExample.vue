d8db9c0d805d9fc4a9d8234f68a0d9c177b474a2
xuliangzhan <xu_liangzhan@163.com>
2026-08-16 10:58:44 +0800

    update docs

--- v4/src/views/grid/keyboard/edit/CodeExample.vue
+++ v4/src/views/grid/keyboard/edit/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="grid/keyboard/edit/Demo1">
+    <CodeLight path="grid/keyboard/edit/Demo1" previewPath="/resource/docsImg/table_keyboard_edit.gif">
       <template #tip>
         <vxe-tip status="primary" title="单元格编辑按键操作"></vxe-tip>
       </template>
