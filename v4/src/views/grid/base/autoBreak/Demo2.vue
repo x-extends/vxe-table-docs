55175f0c70eae04bd77683f1b2508f21b663ce50
xuliangzhan <xu_liangzhan@163.com>
2026-08-21 16:24:01 +0800

    update docs

---
+++ v4/src/views/grid/base/autoBreak/Demo2.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <vxe-switch v-model="gridOptions.showOverflow"></vxe-switch>
+
+    <vxe-grid v-bind="gridOptions"></vxe-grid>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue'
+import type { VxeGridProps } from 'vxe-table'
+
+interface RowVO {
+  id: number
+  name: string
+  role: string
+  sex: string
+  age: number
+  address: string
+}
+
+const gridOptions = reactive<VxeGridProps<RowVO>>({
+  showOverflow: false,
+  cellConfig: {
+    maxHeight: 100
+  },
+  virtualXConfig: {
+    enabled: false
+  },
+  virtualYConfig: {
+    enabled: false
+  },
+  columns: [
+    { type: 'seq', width: 70 },
+    { field: 'name', title: 'Name' },
+    { field: 'sex', title: '列头自动换行列头自动换行行列头自动换行', width: 120 },
+    { field: 'age', title: 'Age' },
+    { field: 'address', title: 'Address' }
+  ],
+  data: [
+    { id: 10001, name: 'Test1', role: 'Develop', sex: 'Man', age: 28, address: 'test abc' },
+    { id: 10002, name: '内容自动换行内容自动换行内容自动换容自动换行内容自动换行内容自动换行内容自动换行内容自动换行内容自动换行内容自动换行内容自动换行内容自动换行容自动换行内容自动换行', role: 'Designer', sex: 'Women', age: 22, address: 'Guangzhou' },
+    { id: 10003, name: 'Test3', role: 'PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM', sex: 'Man', age: 32, address: 'Shanghai' },
+    { id: 10004, name: 'Test4', role: 'Test', sex: 'Women', age: 24, address: 'Shanghai' }
+  ]
+})
+</script>
