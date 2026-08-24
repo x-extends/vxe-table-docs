a42675a16a2ba018a4104544ea07d7ed09801f7f
xuliangzhan <xu_liangzhan@163.com>
2026-08-24 13:21:16 +0800

    update docs

--- v4/src/views/table/base/autoBreak/Demo1.vue
+++ v4/src/views/table/base/autoBreak/Demo1.vue
@@ -2,7 +2,8 @@
   <div>
     <vxe-table
       :show-overflow="false"
-      :data="tableData">
+      :data="tableData"
+    >
       <vxe-column type="seq" width="70"></vxe-column>
       <vxe-column field="name" title="Name"></vxe-column>
       <vxe-column field="role" title="列头自动换行列头自动换行" width="120"></vxe-column>
@@ -25,8 +26,10 @@ interface RowVO {
 
 const tableData = ref<RowVO[]>([
   { id: 10001, name: 'Test1', role: 'Develop', sex: 'Man', age: 28, address: 'test abc' },
-  { id: 10002, name: '内容自动换行内容自动换行内容自动换行内容自动换行内容自动换行', role: 'Designer', sex: 'Women', age: 22, address: 'Guangzhou' },
-  { id: 10003, name: 'Test3', role: 'PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM', sex: 'Man', age: 32, address: 'Shanghai' },
-  { id: 10004, name: 'Test4', role: 'Test', sex: 'Women', age: 24, address: 'Shanghai' }
+  { id: 10002, name: '内容自动换行内容自动换行内容自动换行内容自动换行内容自动换行内容自动换行内容自动换行 内容自动换行内容自动换行内容自动换行内容自动换行内容自动换行', role: 'Designer', sex: 'Women', age: 22, address: 'Guangzhou' },
+  { id: 10003, name: 'Test3', role: 'PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PM PMPM PM PM', sex: 'Man', age: 32, address: 'Shanghai' },
+  { id: 10004, name: 'Test4', role: 'Test', sex: 'Women', age: 24, address: 'Shanghai' },
+  { id: 10005, name: '内容自动换行内容自动换行内容自动换行内容自动换行内容自动换行 内容自动换行内容自动换行内容自动换行内容自动换行自动换行内容自动换行自动换行内容自动换行内容自动换行内容自动换行内容自动换行内容自动换行自动换行内容自动换行自动换行内容自动换行', role: 'Designer', sex: 'Women', age: 22, address: 'Guangzhou' },
+  { id: 10006, name: 'Test3', role: 'DevelopDevelopDevel opDevelop DevelopDevelop DevelopDevelopDevelopDevelop DevelopDevelop DevelopDevelop DevelopDevelop DevelopDevelop', sex: 'Man', age: 32, address: 'Shanghai' }
 ])
 </script>
