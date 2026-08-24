99f4928e4ac4a053799a969fb1a35a030a02ee13
xuliangzhan <xu_liangzhan@163.com>
2026-08-24 11:47:52 +0800

    update docs

--- v3/src/views/grid/export/excludeFields/CodeExample.vue
+++ v3/src/views/grid/export/excludeFields/CodeExample.vue
@@ -5,6 +5,9 @@
         <vxe-tip status="primary" title="排除列">
           通过 <ApiLink name="grid" prop="export-config"/>.<ApiLink name="grid" prop="excludeFields"/> 排除字段
         </vxe-tip>
+        <vxe-tip status="success" title="小提示">
+          默认是导出带有 field 的列
+        </vxe-tip>
       </template>
     </CodeLight>
   </div>
